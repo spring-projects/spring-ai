3835e1e668b003c83a562c98d96c58f8976b412e
Craig Walls <craig@habuma.com>
2023-09-07 15:34:47 -0600

    Add TextLoader implementation of Loader

---
+++ spring-ai-core/src/main/java/org/springframework/ai/loader/impl/TextLoader.java
@@ -0,0 +1,43 @@
+package org.springframework.ai.loader.impl;
+
+import org.springframework.ai.document.Document;
+import org.springframework.ai.loader.Loader;
+import org.springframework.ai.splitter.TextSplitter;
+import org.springframework.ai.splitter.TokenTextSplitter;
+import org.springframework.core.io.Resource;
+import org.springframework.util.StreamUtils;
+
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+public class TextLoader implements Loader {
+
+    private final Resource resource;
+
+    public TextLoader(Resource resource) {
+        Objects.requireNonNull(resource, "The Spring Resource must not be null");
+        this.resource = resource;
+    }
+
+
+    @Override
+    public List<Document> load() {
+        return load(new TokenTextSplitter());
+    }
+
+    @Override
+    public List<Document> load(TextSplitter textSplitter) {
+        try {
+            InputStream inputStream = resource.getInputStream();
+            String doc = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+            return textSplitter.apply(Collections.singletonList(new Document(doc)));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+}
