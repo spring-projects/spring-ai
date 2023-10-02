e68bdeb9a0d74badacaccc1677521fd2612f5b71
imoyb <imoyb@qq.com>
2023-10-02 22:56:45 +0800

    Add OllamaClient implementation of AiClient

---
+++ spring-ai-ollama/README.md
@@ -0,0 +1,9 @@
+## Ollama
+
+Ollama lets you ge tup an running with large language models locally
+
+Refer to the official [README](https://github.com/jmorganca/ollama) to get started.
+
+Note, installing `ollama run llama2` will download a 4GB docker image.
+
+You can run the disabled test in `OllamaClientTests.java` to kick the tires.
---
+++ spring-ai-ollama/src/test/java/org/springframework/ai/ollama/client/OllamaClientTests.java
@@ -0,0 +1,45 @@
+package org.springframework.ai.ollama.client;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.ai.client.AiResponse;
+import org.springframework.ai.prompt.Prompt;
+import org.springframework.util.CollectionUtils;
+
+import java.util.function.Consumer;
+
+public class OllamaClientTests {
+
+	@Test
+	@Disabled("For manual smoke testing only.")
+	public void smokeTest() {
+		OllamaClient ollama2 = getOllamaClient();
+
+		Prompt prompt = new Prompt("Hello");
+		AiResponse aiResponse = ollama2.generate(prompt);
+		Assertions.assertNotNull(aiResponse);
+		Assertions.assertFalse(CollectionUtils.isEmpty(aiResponse.getGenerations()));
+		Assertions.assertFalse(CollectionUtils.isEmpty(aiResponse.getProviderOutput()));
+		Assertions.assertNotNull(aiResponse.getProviderOutput().get("ollama-generate-results"));
+		Assertions.assertNotNull(aiResponse.getGeneration());
+		Assertions.assertNotNull(aiResponse.getGeneration().getText());
+	}
+
+	private static OllamaClient getOllamaClient() {
+		Consumer<OllamaGenerateResult> ollamaGenerateResultConsumer = it -> {
+			if (it.getDone()) {
+				System.out.println();
+				System.out.printf("Total duration: %dms%n", it.getTotalDuration() / 1000 / 1000);
+				System.out.printf("Prompt tokens: %d%n", it.getPromptEvalCount());
+				System.out.printf("Completion tokens: %d%n", it.getEvalCount());
+			}
+			else {
+				System.out.print(it.getResponse());
+			}
+		};
+
+		return new OllamaClient("http://127.0.0.1:11434", "llama2", ollamaGenerateResultConsumer);
+	}
+
+}
