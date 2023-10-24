6030cda59814c2cc6bbb432d3990a4bc61db4574
Christian Tzolov <ctzolov@vmware.com>
2023-10-24 09:25:18 +0200

    Add local, Transformers EmbeddingClient
     - EmbeddingClient implementation that computes, locally, sentence embeddings with SBERT transformers.
     - Uses pre-trained transformer models, serialized into Open Neural Network Exchange (ONNX) format.
     - Deep Java Library and the Microsoft ONNX Java Runtime are used to run
       the ONNX models and compute the embeddings efficiently.
     - Add default tokenizer.json and model.onnx for sentence-transformers/all-MiniLM-L6-v2.
     - Add, configurable resource caching service to allow caching
       remote (http/https) resources to the local FS.
     - README.md provides information on how to serialize ONNX models.
     - add Git LFS configuration for large onnx model files.

---
+++ embedding-clients/transformers-embedding/src/main/java/org/springframework/ai/embedding/ResourceCacheService.java
@@ -0,0 +1,149 @@
+/*
+ * Copyright 2023-2023 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.ai.embedding;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.FileUrlResource;
+import org.springframework.core.io.Resource;
+import org.springframework.util.Assert;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.util.StreamUtils;
+import org.springframework.util.StringUtils;
+
+/**
+ * Service that helps caching remote {@link Resource}s on the local file system.
+ *
+ * @author Christian Tzolov
+ */
+public class ResourceCacheService {
+
+	private static final Log logger = LogFactory.getLog(ResourceCacheService.class);
+
+	/**
+	 * The parent folder that contains all cached resources.
+	 */
+	private final File cacheDirectory;
+
+	/**
+	 * Resources with URI schemas belonging to the excludedUriSchemas are not cached. By
+	 * default the file and classpath resources are not cached as they are already in the
+	 * local file system.
+	 */
+	private List<String> excludedUriSchemas = new ArrayList<>(List.of("file", "classpath"));
+
+	public ResourceCacheService() {
+		this(new File(System.getProperty("java.io.tmpdir"), "spring-ai-onnx-model").getAbsolutePath());
+	}
+
+	public ResourceCacheService(String rootCacheDirectory) {
+		this(new File(rootCacheDirectory));
+	}
+
+	public ResourceCacheService(File rootCacheDirectory) {
+		Assert.notNull(rootCacheDirectory, "Cache directory can not be null.");
+		this.cacheDirectory = rootCacheDirectory;
+		if (!this.cacheDirectory.exists()) {
+			logger.info("Create cache root directory: " + this.cacheDirectory.getAbsolutePath());
+			this.cacheDirectory.mkdirs();
+		}
+		Assert.isTrue(this.cacheDirectory.isDirectory(), "The cache folder must be a directory");
+	}
+
+	/**
+	 * Overrides the excluded URI schemas list.
+	 * @param excludedUriSchemas new list of URI schemas to be excluded from caching.
+	 */
+	public void setExcludedUriSchemas(List<String> excludedUriSchemas) {
+		Assert.notNull(excludedUriSchemas, "The excluded URI schemas list can not be null");
+		this.excludedUriSchemas = excludedUriSchemas;
+	}
+
+	/**
+	 * Get {@link Resource} representing the cached copy of the original resource.
+	 * @param originalResourceUri Resource to be cached.
+	 * @return Returns a cached resource. If the original resource's URI schema is within
+	 * the excluded schema list the original resource is returned.
+	 */
+	public Resource getCachedResource(String originalResourceUri) {
+		return this.getCachedResource(new DefaultResourceLoader().getResource(originalResourceUri));
+	}
+
+	/**
+	 * Get {@link Resource} representing the cached copy of the original resource.
+	 * @param originalResource Resource to be cached.
+	 * @return Returns a cached resource. If the original resource's URI schema is within
+	 * the excluded schema list the original resource is returned.
+	 */
+	public Resource getCachedResource(Resource originalResource) {
+		try {
+			if (this.excludedUriSchemas.contains(originalResource.getURI().getScheme())) {
+				logger.info("The " + originalResource.toString() + " resource with URI schema ["
+						+ originalResource.getURI().getScheme() + "] is excluded from caching");
+				return originalResource;
+			}
+
+			File cachedFile = getCachedFile(originalResource);
+			if (!cachedFile.exists()) {
+				FileCopyUtils.copy(StreamUtils.copyToByteArray(originalResource.getInputStream()), cachedFile);
+				logger.info("Caching the " + originalResource.toString() + " resource to: " + cachedFile);
+			}
+			return new FileUrlResource(cachedFile.getAbsolutePath());
+		}
+		catch (Exception e) {
+			throw new IllegalStateException("Failed to cache the resource: " + originalResource.getDescription(), e);
+		}
+	}
+
+	private File getCachedFile(Resource originalResource) throws IOException {
+		var resourceParentFolder = new File(this.cacheDirectory,
+				UUID.nameUUIDFromBytes(pathWithoutLastSegment(originalResource.getURI())).toString());
+		resourceParentFolder.mkdirs();
+		String newFileName = getCacheName(originalResource);
+		return new File(resourceParentFolder, newFileName);
+	}
+
+	private byte[] pathWithoutLastSegment(URI uri) {
+		String path = uri.toASCIIString();
+		var pathBeforeLastSegment = path.substring(0, path.lastIndexOf('/') + 1);
+		return pathBeforeLastSegment.getBytes();
+	}
+
+	private String getCacheName(Resource originalResource) throws IOException {
+		String fileName = originalResource.getFilename();
+		String fragment = originalResource.getURI().getFragment();
+		return !StringUtils.hasText(fragment) ? fileName : fileName + "_" + fragment;
+	}
+
+	public void deleteCacheFolder() {
+		if (this.cacheDirectory.exists()) {
+			logger.info("Empty Model Cache at:" + this.cacheDirectory.getAbsolutePath());
+			this.cacheDirectory.delete();
+			this.cacheDirectory.mkdirs();
+		}
+	}
+
+}
---
+++ embedding-clients/transformers-embedding/src/main/java/org/springframework/ai/embedding/TransformersEmbeddingClient.java
@@ -0,0 +1,274 @@
+package org.springframework.ai.embedding;
+
+import java.nio.FloatBuffer;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import ai.djl.huggingface.tokenizers.Encoding;
+import ai.djl.huggingface.tokenizers.HuggingFaceTokenizer;
+import ai.djl.ndarray.NDArray;
+import ai.djl.ndarray.NDManager;
+import ai.djl.ndarray.types.DataType;
+import ai.djl.ndarray.types.Shape;
+import ai.onnxruntime.OnnxTensor;
+import ai.onnxruntime.OnnxValue;
+import ai.onnxruntime.OrtEnvironment;
+import ai.onnxruntime.OrtException;
+import ai.onnxruntime.OrtSession;
+
+import org.springframework.ai.document.Document;
+import org.springframework.ai.document.MetadataMode;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.Resource;
+import org.springframework.util.Assert;
+import org.springframework.util.StringUtils;
+
+/**
+ * https://www.sbert.net/index.html https://www.sbert.net/docs/pretrained_models.html
+ *
+ * @author Christian Tzolov
+ */
+public class TransformersEmbeddingClient implements EmbeddingClient, InitializingBean {
+
+	// ONNX tokenizer for the all-MiniLM-L6-v2 model
+	private final static String DEFAULT_ONNX_TOKENIZER_URI = "https://raw.githubusercontent.com/spring-projects-experimental/spring-ai/main/embedding-clients/transformers-embedding/src/main/resources/onnx/all-MiniLM-L6-v2/tokenizer.json";
+
+	// ONNX model for all-MiniLM-L6-v2 pre-trained transformer:
+	// https://huggingface.co/sentence-transformers/all-MiniLM-L6-v2
+	private final static String DEFAULT_ONNX_MODEL_URI = "https://raw.githubusercontent.com/spring-projects-experimental/spring-ai/main/embedding-clients/transformers-embedding/src/main/resources/onnx/all-MiniLM-L6-v2/model.onnx";
+
+	private final static int EMBEDDING_AXIS = 1;
+
+	private Resource tokenizerResource = toResource(DEFAULT_ONNX_TOKENIZER_URI);
+
+	private Resource modelResource = toResource(DEFAULT_ONNX_MODEL_URI);
+
+	private int gpuDeviceId = -1;
+
+	/**
+	 * DJL, Huggingface tokenizer implementation of the {@link Tokenizer} interface that
+	 * converts sentences into token.
+	 */
+	private HuggingFaceTokenizer tokenizer;
+
+	/**
+	 * ONNX runtime configurations: https://onnxruntime.ai/docs/get-started/with-java.html
+	 */
+	private OrtEnvironment environment;
+
+	private OrtSession session;
+
+	private final AtomicInteger embeddingDimensions = new AtomicInteger(-1);
+
+	private final MetadataMode metadataMode;
+
+	/**
+	 * Resource cache directory. Used to cache remote resources, such as the ONNX models,
+	 * to the local file system.
+	 */
+	private String resourceCacheDirectory;
+
+	/**
+	 * Allow disabling the resource caching.
+	 */
+	private boolean disableCaching = false;
+
+	private ResourceCacheService cache;
+
+	public TransformersEmbeddingClient() {
+		this(MetadataMode.NONE);
+	}
+
+	public TransformersEmbeddingClient(MetadataMode metadataMode) {
+		Assert.notNull(metadataMode, "Metadata mode should not be null");
+		this.metadataMode = metadataMode;
+	}
+
+	public void setDisableCaching(boolean disableCaching) {
+		this.disableCaching = disableCaching;
+	}
+
+	public void setResourceCacheDirectory(String resourceCacheDir) {
+		this.resourceCacheDirectory = resourceCacheDir;
+	}
+
+	public void setGpuDeviceId(int gpuDeviceId) {
+		this.gpuDeviceId = gpuDeviceId;
+	}
+
+	public void setTokenizerResource(Resource tokenizerResource) {
+		this.tokenizerResource = tokenizerResource;
+	}
+
+	public void setModelResource(Resource modelResource) {
+		this.modelResource = modelResource;
+	}
+
+	public void setTokenizerResource(String tokenizerResourceUri) {
+		this.tokenizerResource = toResource(tokenizerResourceUri);
+	}
+
+	public void setModelResource(String modelResourceUri) {
+		this.modelResource = toResource(modelResourceUri);
+	}
+
+	public void setEmbeddingDimensions(int dimension) {
+		this.embeddingDimensions.set(dimension);
+	}
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		this.cache = StringUtils.hasText(this.resourceCacheDirectory)
+				? new ResourceCacheService(this.resourceCacheDirectory) : new ResourceCacheService();
+		this.tokenizer = HuggingFaceTokenizer.newInstance(getCachedResource(this.tokenizerResource).getInputStream(),
+				Map.of());
+		this.environment = OrtEnvironment.getEnvironment();
+
+		var sessionOptions = new OrtSession.SessionOptions();
+		if (this.gpuDeviceId >= 0) {
+			// Run on a GPU or with another provider
+			sessionOptions.addCUDA(this.gpuDeviceId);
+		}
+		this.session = this.environment.createSession(getCachedResource(this.modelResource).getContentAsByteArray(),
+				sessionOptions);
+	}
+
+	private Resource getCachedResource(Resource resource) {
+		return this.disableCaching ? resource : this.cache.getCachedResource(resource);
+	}
+
+	@Override
+	public List<Double> embed(String text) {
+		return embed(List.of(text)).get(0);
+	}
+
+	@Override
+	public List<Double> embed(Document document) {
+		return this.embed(document.getFormattedContent(this.metadataMode));
+	}
+
+	@Override
+	public EmbeddingResponse embedForResponse(List<String> texts) {
+		List<Embedding> data = new ArrayList<>();
+		List<List<Double>> embed = this.embed(texts);
+		for (int i = 0; i < embed.size(); i++) {
+			data.add(new Embedding(embed.get(i), i));
+		}
+		return new EmbeddingResponse(data, Map.of());
+	}
+
+	@Override
+	public List<List<Double>> embed(List<String> texts) {
+
+		List<List<Double>> resultEmbeddings = new ArrayList<>();
+
+		try {
+
+			Encoding[] encodings = this.tokenizer.batchEncode(texts);
+
+			long[][] input_ids0 = new long[encodings.length][];
+			long[][] attention_mask0 = new long[encodings.length][];
+			long[][] token_type_ids0 = new long[encodings.length][];
+
+			for (int i = 0; i < encodings.length; i++) {
+				input_ids0[i] = encodings[i].getIds();
+				attention_mask0[i] = encodings[i].getAttentionMask();
+				token_type_ids0[i] = encodings[i].getTypeIds();
+			}
+
+			OnnxTensor inputIds = OnnxTensor.createTensor(this.environment, input_ids0);
+			OnnxTensor attentionMask = OnnxTensor.createTensor(this.environment, attention_mask0);
+			OnnxTensor tokenTypeIds = OnnxTensor.createTensor(this.environment, token_type_ids0);
+
+			Map<String, OnnxTensor> modelInputs = Map.of("input_ids", inputIds, "attention_mask", attentionMask,
+					"token_type_ids", tokenTypeIds);
+
+			try (OrtSession.Result results = this.session.run(modelInputs)) {
+
+				// OnnxValue lastHiddenState = results.get(0);
+				OnnxValue lastHiddenState = results.get("last_hidden_state").get();
+
+				// 0 - batch_size (1..x)
+				// 1 - sequence_length (128)
+				// 2 - embedding dimensions (384)
+				float[][][] tokenEmbeddings = (float[][][]) lastHiddenState.getValue();
+
+				try (NDManager manager = NDManager.newBaseManager()) {
+					NDArray ndTokenEmbeddings = create(tokenEmbeddings, manager);
+					NDArray ndAttentionMask = manager.create(attention_mask0);
+
+					NDArray embedding = meanPooling(ndTokenEmbeddings, ndAttentionMask);
+
+					for (int i = 0; i < embedding.size(0); i++) {
+						resultEmbeddings.add(toDoubleList(embedding.get(i).toFloatArray()));
+					}
+				}
+			}
+		}
+		catch (OrtException ex) {
+			throw new RuntimeException(ex);
+		}
+
+		return resultEmbeddings;
+	}
+
+	// Build a NDArray from 3D float array.
+	private NDArray create(float[][][] data3d, NDManager manager) {
+
+		FloatBuffer buffer = FloatBuffer.allocate(data3d.length * data3d[0].length * data3d[0][0].length);
+
+		for (float[][] data2d : data3d) {
+			for (float[] data1d : data2d) {
+				buffer.put(data1d);
+			}
+		}
+		buffer.rewind();
+
+		return manager.create(buffer, new Shape(data3d.length, data3d[0].length, data3d[0][0].length));
+	}
+
+	private NDArray meanPooling(NDArray tokenEmbeddings, NDArray attentionMask) {
+
+		NDArray attentionMaskExpanded = attentionMask.expandDims(-1)
+			.broadcast(tokenEmbeddings.getShape())
+			.toType(DataType.FLOAT32, false);
+
+		// Multiply token embeddings with expanded attention mask
+		NDArray weightedEmbeddings = tokenEmbeddings.mul(attentionMaskExpanded);
+
+		// Sum along the appropriate axis
+		NDArray sumEmbeddings = weightedEmbeddings.sum(new int[] { EMBEDDING_AXIS });
+
+		// Clamp the attention mask sum to avoid division by zero
+		NDArray sumMask = attentionMaskExpanded.sum(new int[] { EMBEDDING_AXIS }).clip(1e-9f, Float.MAX_VALUE);
+
+		// Divide sum embeddings by sum mask
+		return sumEmbeddings.div(sumMask);
+	}
+
+	private List<Double> toDoubleList(float[] floats) {
+		List<Double> result = new ArrayList<>();
+		if (floats != null && floats.length > 0) {
+			for (int i = 0; i < floats.length; i++) {
+				result.add((double) floats[i]);
+			}
+		}
+		return result;
+	}
+
+	@Override
+	public int dimensions() {
+		if (this.embeddingDimensions.get() < 0) {
+			this.embeddingDimensions.set(EmbeddingUtil.dimensions(this, "Test"));
+		}
+		return this.embeddingDimensions.get();
+	}
+
+	private static Resource toResource(String uri) {
+		return new DefaultResourceLoader().getResource(uri);
+	}
+
+}
