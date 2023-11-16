6bdea65cd50759249db509fbaa3f5393be741f95
Tommy Ludwig <8924140+shakuzen@users.noreply.github.com>
2023-11-16 23:23:20 +0900

    Auto-configuration and starter for Ollama
     - Adds auto-configuration and a starter for Ollama.
     - Add testconteiner orca-mini integration test.
     - Improve code style and documentation.
    
    Resolves #68, Resolves #69

--- spring-ai-ollama/README.md
+++ spring-ai-ollama/README.md
@@ -1,9 +1,56 @@
-## Ollama
+# Ollama
 
-Ollama lets you ge tup an running with large language models locally
+Ollama lets you get up and running with large language models locally.
 
 Refer to the official [README](https://github.com/jmorganca/ollama) to get started.
 
 Note, installing `ollama run llama2` will download a 4GB docker image.
 
 You can run the disabled test in `OllamaClientTests.java` to kick the tires.
+
+## How to use
+
+Add the `spring-ai-ollama` dependency to your project's pom:
+
+```xml
+<dependency>
+   <groupId>org.springframework.experimental.ai</groupId>
+   <artifactId>spring-ai-ollama</artifactId>
+   <version>0.7.1-SNAPSHOT</version>
+</dependency>
+```
+
+then create an client and use generate response:
+
+```java
+var ollamaClient = new OllamaClient("http://127.0.0.1:11434", "llama2",
+      ollamaResult -> {
+         if (ollamaResult.getDone()) {
+            ....
+         }
+      });
+
+AiResponse aiResponse = ollamaClient.generate(new Prompt("Hello"));
+```
+
+### Spring Boot Starter
+
+For convenience you can opt for the Ollama Boot starter.
+For this add the following dependency:
+
+```xml
+<dependency>
+   <groupId>org.springframework.experimental.ai</groupId>
+   <artifactId>spring-ai-ollama-spring-boot-starter</artifactId>
+   <version>0.7.1-SNAPSHOT</version>
+</dependency>
+```
+
+and use the `spring.ai.ollama.*` properties to configure it if you want to use something other than the default values.
+
+The complete list of supported properties are:
+
+| Property    | Description | Default |
+| -------- | ----- | ----- |
+| spring.ai.ollama.base-url | Base URL where Ollama API server is running. | `http://localhost:11434` |
+| spring.ai.ollama.model | Language model to use. | `llama2` |
---
+++ spring-ai-spring-boot-autoconfigure/src/test/java/org/springframework/ai/autoconfigure/ollama/OllamaAutoConfigurationIT.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2023-2023 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.ai.autoconfigure.ollama;
+
+import java.util.Map;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import org.springframework.ai.ollama.client.OllamaClient;
+import org.springframework.boot.autoconfigure.AutoConfigurations;
+import org.springframework.boot.test.context.runner.ApplicationContextRunner;
+import org.springframework.http.HttpEntity;
+import org.springframework.web.client.RestTemplate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Disabled("As it downloads the 3GB 'orca-mini' it can take couple of minutes to initialize.")
+@Testcontainers
+public class OllamaAutoConfigurationIT {
+
+	private static final Log logger = LogFactory.getLog(OllamaAutoConfigurationIT.class);
+
+	@Container
+	static GenericContainer<?> ollamaContainer = new GenericContainer<>("ollama/ollama").withExposedPorts(11434);
+
+	private final ApplicationContextRunner contextRunner = new ApplicationContextRunner()
+		.withPropertyValues("spring.ai.ollama.baseUrl=http://localhost:" + ollamaContainer.getMappedPort(11434),
+				"spring.ai.ollama.model=orca-mini")
+		.withConfiguration(AutoConfigurations.of(OllamaAutoConfiguration.class));
+
+	@BeforeAll
+	public static void beforeAll() {
+		logger.info("Start pulling the 'orca-mini' model (3GB) ... would take several minutes ...");
+		new RestTemplate().postForLocation("http://localhost:{port}/api/pull",
+				new HttpEntity<>(Map.of("name", "orca-mini")), ollamaContainer.getMappedPort(11434));
+		logger.info("orca-mini pulling competed!");
+	}
+
+	@Test
+	void generate() {
+		contextRunner.run(context -> {
+			OllamaClient client = context.getBean(OllamaClient.class);
+			assertThat(client.getBaseUrl()).isEqualTo("http://localhost:" + ollamaContainer.getMappedPort(11434));
+			assertThat(client.getModel()).isEqualTo("orca-mini");
+
+			String response = client.generate("Hello");
+
+			assertThat(response).isNotEmpty();
+			logger.info("Response: " + response);
+		});
+	}
+
+}
