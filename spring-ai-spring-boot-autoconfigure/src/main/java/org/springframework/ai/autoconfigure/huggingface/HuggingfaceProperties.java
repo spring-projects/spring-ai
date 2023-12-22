b73a9ea6f9ae52f162e002d13b26884f67dbcab2
Mark Pollack <mpollack@vmware.com>
2023-12-22 09:02:05 -0500

    fix prefix in configuration properties for hugging face

--- spring-ai-spring-boot-autoconfigure/src/main/java/org/springframework/ai/autoconfigure/huggingface/HuggingfaceProperties.java
+++ spring-ai-spring-boot-autoconfigure/src/main/java/org/springframework/ai/autoconfigure/huggingface/HuggingfaceProperties.java
@@ -2,9 +2,7 @@ package org.springframework.ai.autoconfigure.huggingface;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-import static org.springframework.ai.autoconfigure.openai.OpenAiProperties.CONFIG_PREFIX;
-
-@ConfigurationProperties(CONFIG_PREFIX)
+@ConfigurationProperties(HuggingfaceProperties.CONFIG_PREFIX)
 public class HuggingfaceProperties {
 
 	public static final String CONFIG_PREFIX = "spring.ai.huggingface";
