e2ed8f5797aa3972fd08828d78b2fb8b5bac905b
Christian Tzolov <ctzolov@vmware.com>
2024-01-24 15:41:13 +0100

    Add missing code license blocks

--- models/spring-ai-openai/src/test/java/org/springframework/ai/openai/image/OpenAiImageClientWithImageResponseMetadataTests.java
+++ models/spring-ai-openai/src/test/java/org/springframework/ai/openai/image/OpenAiImageClientWithImageResponseMetadataTests.java
@@ -63,7 +63,7 @@ public class OpenAiImageClientWithImageResponseMetadataTests {
 	}
 
 	@Test
-	void aiResponseContainsImageRespoinseMetadata() {
+	void aiResponseContainsImageResponseMetadata() {
 
 		prepareMock();
 
--- spring-ai-core/src/main/java/org/springframework/ai/model/ModelClient.java
+++ spring-ai-core/src/main/java/org/springframework/ai/model/ModelClient.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2024-2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.springframework.ai.model;
 
 /**
@@ -17,8 +33,6 @@ public interface ModelClient<TReq extends ModelRequest<?>, TRes extends ModelRes
 	/**
 	 * Executes a method call to the AI model.
 	 * @param request the request object to be sent to the AI model
-	 * @param <TReq> the generic type of the request object
-	 * @param <TRes> the generic type of the response from the AI model
 	 * @return the response from the AI model
 	 */
 	TRes call(TReq request);
