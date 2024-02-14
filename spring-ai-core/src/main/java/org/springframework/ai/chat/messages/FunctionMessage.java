ca0d10d67863dff550a47edc5ae86f529977eccc
Christian Tzolov <ctzolov@vmware.com>
2024-02-14 07:18:45 +0100

    Resolve #324

--- spring-ai-core/src/main/java/org/springframework/ai/chat/messages/FunctionMessage.java
+++ spring-ai-core/src/main/java/org/springframework/ai/chat/messages/FunctionMessage.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 the original author or authors.
+ * Copyright 2023-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,11 +21,11 @@ import java.util.Map;
 public class FunctionMessage extends AbstractMessage {
 
 	public FunctionMessage(String content) {
-		super(MessageType.SYSTEM, content);
+		super(MessageType.FUNCTION, content);
 	}
 
 	public FunctionMessage(String content, Map<String, Object> properties) {
-		super(MessageType.SYSTEM, content, properties);
+		super(MessageType.FUNCTION, content, properties);
 	}
 
 	@Override
