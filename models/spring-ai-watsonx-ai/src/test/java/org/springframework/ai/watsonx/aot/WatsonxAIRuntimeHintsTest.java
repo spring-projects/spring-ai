47eb9409b120e277b472fdb1abbaf184167bc772
PabloSanchi <pablo.sanchi.herrera@gmail.com>
2024-02-28 21:39:52 +0100

    feat: add runtime hints tests

---
+++ models/spring-ai-watsonx-ai/src/test/java/org/springframework/ai/watsonx/aot/WatsonxAIRuntimeHintsTest.java
@@ -0,0 +1,35 @@
+package org.springframework.ai.watsonx.aot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.ai.watsonx.api.WatsonxAIApi;
+import org.springframework.ai.watsonx.api.WatsonxAIOptions;
+import org.springframework.aot.hint.RuntimeHints;
+import org.springframework.aot.hint.TypeReference;
+
+import java.util.Set;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.springframework.ai.aot.AiRuntimeHints.findJsonAnnotatedClassesInPackage;
+import static org.springframework.aot.hint.predicate.RuntimeHintsPredicates.reflection;
+
+public class WatsonxAIRuntimeHintsTest {
+
+    @Test
+    void registerHints() {
+        RuntimeHints runtimeHints = new RuntimeHints();
+        WatsonxAIRuntimeHints watsonxAIRuntimeHintsTest = new WatsonxAIRuntimeHints();
+        watsonxAIRuntimeHintsTest.registerHints(runtimeHints, null);
+
+        Set<TypeReference> jsonAnnotatedClasses = findJsonAnnotatedClassesInPackage(WatsonxAIApi.class);
+        for (TypeReference jsonAnnotatedClass : jsonAnnotatedClasses) {
+            assertThat(runtimeHints).matches(reflection().onType(jsonAnnotatedClass));
+        }
+
+        jsonAnnotatedClasses = findJsonAnnotatedClassesInPackage(WatsonxAIOptions.class);
+        for (TypeReference jsonAnnotatedClass : jsonAnnotatedClasses) {
+            assertThat(runtimeHints).matches(reflection().onType(jsonAnnotatedClass));
+        }
+    }
+
+
+}
