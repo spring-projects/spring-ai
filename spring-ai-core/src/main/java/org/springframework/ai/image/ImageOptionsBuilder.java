092b6a9ebac14e6738c512fe9742c0e5358a7ded
youngpar <youngseo321@gmail.com>
2024-03-08 00:44:08 +0900

    refactor ImageOptionsBuilder.java
    Hidden Implementation and Enforced Access through an Interface.

--- spring-ai-core/src/main/java/org/springframework/ai/image/ImageOptionsBuilder.java
+++ spring-ai-core/src/main/java/org/springframework/ai/image/ImageOptionsBuilder.java
@@ -15,27 +15,108 @@
  */
 package org.springframework.ai.image;
 
+/**
+ * Builder for {@link ImageOptions}. This builder creates option objects required for
+ * image generation.
+ *
+ * @author youngmon
+ * @since 0.8.1
+ */
 public class ImageOptionsBuilder {
 
-	private class ImageModelOptionsImpl implements ImageOptions {
+	private Integer n;
 
-		private Integer n;
+	private String model;
 
-		private String model;
+	private Integer width;
 
-		private Integer width;
+	private Integer height;
 
-		private Integer height;
+	private String responseFormat;
 
-		private String responseFormat;
+	private ImageOptionsBuilder() {
+	}
+
+	/**
+	 * Creates a new {@link ImageOptionsBuilder} instance.
+	 * @return A new instance of ImageOptionsBuilder.
+	 */
+	public static ImageOptionsBuilder builder() {
+		return new ImageOptionsBuilder();
+	}
+
+	/**
+	 * Initializes a new {@link ImageOptionsBuilder} with settings from an existing
+	 * {@link ImageOptions} object.
+	 * @param options The ImageOptions object whose settings are to be used.
+	 * @return A ImageOptionsBuilder instance initialized with the provided ImageOptions
+	 * settings.
+	 */
+	public static ImageOptionsBuilder builder(final ImageOptions options) {
+		return builder().withN(options.getN())
+			.withModel(options.getModel())
+			.withHeight(options.getHeight())
+			.withWidth(options.getWidth())
+			.withResponseFormat(options.getResponseFormat());
+	}
+
+	public ImageOptions build() {
+		return new ImageOptionsImpl(this.n, this.model, this.width, this.height, this.responseFormat);
+	}
+
+	public ImageOptionsBuilder withN(final Integer n) {
+		this.n = n;
+		return this;
+	}
+
+	public ImageOptionsBuilder withModel(final String model) {
+		this.model = model;
+		return this;
+	}
+
+	public ImageOptionsBuilder withResponseFormat(final String responseFormat) {
+		this.responseFormat = responseFormat;
+		return this;
+	}
+
+	public ImageOptionsBuilder withWidth(final Integer width) {
+		this.width = width;
+		return this;
+	}
+
+	public ImageOptionsBuilder withHeight(final Integer height) {
+		this.height = height;
+		return this;
+	}
+
+	/**
+	 * Created only by ImageOptionsBuilder for controlled setup. Hidden implementation,
+	 * accessed via ImageOptions interface. Promotes modularity and easy use.
+	 */
+	private class ImageOptionsImpl implements ImageOptions {
+
+		private final Integer n;
+
+		private final String model;
+
+		private final Integer width;
+
+		private final Integer height;
+
+		private final String responseFormat;
+
+		ImageOptionsImpl(final Integer n, final String model, final Integer width, final Integer height,
+				final String responseFormat) {
+			this.n = n;
+			this.model = model;
+			this.width = width;
+			this.height = height;
+			this.responseFormat = responseFormat;
+		}
 
 		@Override
 		public Integer getN() {
 			return n;
-		}
-
-		public void setN(Integer n) {
-			this.n = n;
 		}
 
 		@Override
@@ -43,17 +124,9 @@ public class ImageOptionsBuilder {
 			return model;
 		}
 
-		public void setModel(String model) {
-			this.model = model;
-		}
-
 		@Override
 		public String getResponseFormat() {
 			return responseFormat;
-		}
-
-		public void setResponseFormat(String responseFormat) {
-			this.responseFormat = responseFormat;
 		}
 
 		@Override
@@ -61,58 +134,11 @@ public class ImageOptionsBuilder {
 			return width;
 		}
 
-		public void setWidth(Integer width) {
-			this.width = width;
-		}
-
 		@Override
 		public Integer getHeight() {
 			return height;
 		}
 
-		public void setHeight(Integer height) {
-			this.height = height;
-		}
-
-	}
-
-	private final ImageModelOptionsImpl options = new ImageModelOptionsImpl();
-
-	private ImageOptionsBuilder() {
-
-	}
-
-	public static ImageOptionsBuilder builder() {
-		return new ImageOptionsBuilder();
-	}
-
-	public ImageOptionsBuilder withN(Integer n) {
-		options.setN(n);
-		return this;
-	}
-
-	public ImageOptionsBuilder withModel(String model) {
-		options.setModel(model);
-		return this;
-	}
-
-	public ImageOptionsBuilder withResponseFormat(String responseFormat) {
-		options.setResponseFormat(responseFormat);
-		return this;
-	}
-
-	public ImageOptionsBuilder withWidth(Integer width) {
-		options.setWidth(width);
-		return this;
-	}
-
-	public ImageOptionsBuilder withHeight(Integer height) {
-		options.setHeight(height);
-		return this;
-	}
-
-	public ImageOptions build() {
-		return options;
 	}
 
 }
