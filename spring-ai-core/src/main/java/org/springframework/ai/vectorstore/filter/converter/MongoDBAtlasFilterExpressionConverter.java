510a2ce22f699bd09ae61f0a231b73ab10ceb01e
Chris Smith <wingedkraby@gmail.com>
2024-03-08 16:25:04 -0500

    finish implementing filter support

--- spring-ai-core/src/main/java/org/springframework/ai/vectorstore/filter/converter/MongoDBAtlasFilterExpressionConverter.java
+++ spring-ai-core/src/main/java/org/springframework/ai/vectorstore/filter/converter/MongoDBAtlasFilterExpressionConverter.java
@@ -5,40 +5,68 @@ import org.springframework.ai.vectorstore.filter.FilterExpressionConverter;
 
 public class MongoDBAtlasFilterExpressionConverter extends AbstractFilterExpressionConverter {
 
-    @Override
-    protected void doExpression(Filter.Expression expression, StringBuilder context) {
+	@Override
+	protected void doExpression(Filter.Expression expression, StringBuilder context) {
+		// Handling AND/OR
+		if (Filter.ExpressionType.AND.equals(expression.type()) || Filter.ExpressionType.OR.equals(expression.type())) {
+			doCompoundExpressionType(expression, context);
+		}
+		else {
+			doSingleExpressionType(expression, context);
+		}
+	}
 
-    }
+	private void doCompoundExpressionType(Filter.Expression expression, StringBuilder context) {
+		context.append("{");
+		context.append(getOperationSymbol(expression));
+		context.append(":[");
+		this.convertOperand(expression.left(), context);
+		context.append(",");
+		this.convertOperand(expression.right(), context);
+		context.append("]}");
+	}
 
-    private String getOperationSymbol(Filter.Expression exp) {
-        switch (exp.type()) {
-            case AND:
-                return "$and";
-            case OR:
-                return "$or";
-            case EQ:
-                return "$eq";
-            case NE:
-                return "$ne";
-            case LT:
-                return "$lt";
-            case LTE:
-                return "$lte";
-            case GT:
-                return "$gt";
-            case GTE:
-                return "$gte";
-            case IN:
-                return "$in";
-            case NIN:
-                return "$nin ";
-            default:
-                throw new RuntimeException("Not supported expression type:" + exp.type());
-        }
-    }
-    @Override
-    protected void doKey(Filter.Key filterKey, StringBuilder context) {
-        var identifier = (hasOuterQuotes(filterKey.key())) ? removeOuterQuotes(filterKey.key()) : filterKey.key();
-        context.append("\"metadata." + identifier + "\"");
-    }
+	private void doSingleExpressionType(Filter.Expression expression, StringBuilder context) {
+		context.append("{");
+		this.convertOperand(expression.left(), context);
+		context.append(":{");
+		context.append(getOperationSymbol(expression));
+		context.append(":");
+		this.convertOperand(expression.right(), context);
+		context.append("}}");
+	}
+
+	private String getOperationSymbol(Filter.Expression exp) {
+		switch (exp.type()) {
+			case AND:
+				return "$and";
+			case OR:
+				return "$or";
+			case EQ:
+				return "$eq";
+			case NE:
+				return "$ne";
+			case LT:
+				return "$lt";
+			case LTE:
+				return "$lte";
+			case GT:
+				return "$gt";
+			case GTE:
+				return "$gte";
+			case IN:
+				return "$in";
+			case NIN:
+				return "$nin";
+			default:
+				throw new RuntimeException("Not supported expression type:" + exp.type());
+		}
+	}
+
+	@Override
+	protected void doKey(Filter.Key filterKey, StringBuilder context) {
+		var identifier = (hasOuterQuotes(filterKey.key())) ? removeOuterQuotes(filterKey.key()) : filterKey.key();
+		context.append("\"metadata." + identifier + "\"");
+	}
+
 }
