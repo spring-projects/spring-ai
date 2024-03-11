9bb781c7a11ce7e6be9ffb64e51e9c13f20d0c7f
youngpar <youngseo321@gmail.com>
2024-03-11 21:40:45 +0900

    refactor ChatMessage.java
    apply Builder pattern

--- spring-ai-core/src/main/java/org/springframework/ai/chat/messages/ChatMessage.java
+++ spring-ai-core/src/main/java/org/springframework/ai/chat/messages/ChatMessage.java
@@ -15,24 +15,100 @@
  */
 package org.springframework.ai.chat.messages;
 
+import java.util.List;
 import java.util.Map;
 
 public class ChatMessage extends AbstractMessage {
 
-	public ChatMessage(String role, String content) {
-		super(MessageType.valueOf(role), content);
+	private ChatMessage(final MessageType type, final String textContent, final List<MediaData> mediaData,
+			final Map<String, Object> properties) {
+		super(type, textContent, mediaData, properties);
 	}
 
-	public ChatMessage(String role, String content, Map<String, Object> properties) {
-		super(MessageType.valueOf(role), content, properties);
+	/**
+	 * Creates a new {@link ChatMessageBuilder} instance.
+	 * @return A new instance of ChatMessageBuilder.
+	 */
+	public static ChatMessageBuilder builder() {
+		return new ChatMessageBuilder();
 	}
 
-	public ChatMessage(MessageType messageType, String content) {
-		super(messageType, content);
+	/**
+	 * Initializes a new {@link ChatMessageBuilder} with settings from an existing
+	 * {@link ChatMessage} object.
+	 * @param message The ChatMessage object whose settings are to be used.
+	 * @return A ChatMessageBuilder instance initialized with the provided ChatMessage
+	 * settings.
+	 */
+	public static ChatMessageBuilder builder(final ChatMessage message) {
+		return builder().withMessageType(message.getMessageType())
+			.withContent(message.getContent())
+			.withProperties(message.getProperties());
 	}
 
-	public ChatMessage(MessageType messageType, String content, Map<String, Object> properties) {
-		super(messageType, content, properties);
+	/**
+	 * Initializes a {@link ChatMessageBuilder} with a message type determined by the
+	 * provided role. This method is a convenient shortcut to start building a
+	 * {@link ChatMessage} when the role is known. It internally calls {@link #builder()}
+	 * to create a new builder instance and sets the message type by converting the role
+	 * to a {@link MessageType} using {@link MessageType#valueOf(String)}.
+	 *
+	 * See {@link ChatMessageBuilder#withRole(String)} for more details on role handling.
+	 * @param role a string that should match one of the {@link MessageType} enum
+	 * constants, indicating the desired message type
+	 * @return a {@link ChatMessageBuilder} instance with the initial message type set
+	 * @throws IllegalArgumentException if the role does not correspond to any
+	 * {@link MessageType} enum constants
+	 */
+	public static ChatMessageBuilder builder(final String role) {
+		return builder().withMessageType(MessageType.valueOf(role));
+	}
+
+	/**
+	 * Builder for {@link ChatMessage}. This builder creates chat message object.
+	 */
+	public static class ChatMessageBuilder extends AbstractMessageBuilder<ChatMessageBuilder> {
+
+		private ChatMessageBuilder() {
+			super();
+		}
+
+		/**
+		 * Sets the message type based on the provided role. The role argument is expected
+		 * to correspond to a valid enum value of {@link MessageType}. This method allows
+		 * for the dynamic assignment of the message type, facilitating different
+		 * behaviors or processing logic based on the message's role in the conversation.
+		 *
+		 * Note: This method internally converts the role string to a {@link MessageType}
+		 * enum. It's important to ensure that the provided role string exactly matches
+		 * one of the enum's constants. If the match is unsuccessful, an
+		 * IllegalArgumentException will be thrown by {@link MessageType#valueOf(String)}.
+		 * @param role a string representation of the message type, corresponding to the
+		 * {@link MessageType} enum
+		 * @return this builder instance to allow for method chaining
+		 * @throws IllegalArgumentException if the provided role does not match any
+		 * {@link MessageType} enum constants
+		 */
+		public ChatMessageBuilder withRole(final String role) {
+			return super.withMessageType(MessageType.valueOf(role));
+		}
+
+		public ChatMessageBuilder withMessageType(final MessageType type) {
+			return super.withMessageType(type);
+		}
+
+		public ChatMessageBuilder withContent(final String content) {
+			return super.withContent(content);
+		}
+
+		public ChatMessageBuilder withProperties(final Map<String, Object> properties) {
+			return super.withProperties(properties);
+		}
+
+		public ChatMessage build() {
+			return new ChatMessage(this.messageType, this.textContent, this.mediaData, this.properties);
+		}
+
 	}
 
 }
