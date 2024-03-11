4d03fa008d300b621e632d1a15492d3f42fc5656
youngpar <youngseo321@gmail.com>
2024-03-11 21:38:17 +0900

    refactor AssistantMessage.java
    apply Builder Pattern

--- spring-ai-core/src/main/java/org/springframework/ai/chat/messages/AssistantMessage.java
+++ spring-ai-core/src/main/java/org/springframework/ai/chat/messages/AssistantMessage.java
@@ -15,6 +15,7 @@
  */
 package org.springframework.ai.chat.messages;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -25,12 +26,52 @@ import java.util.Map;
  */
 public class AssistantMessage extends AbstractMessage {
 
-	public AssistantMessage(String content) {
-		super(MessageType.ASSISTANT, content);
+	private AssistantMessage(final MessageType type, final String textContent, final List<MediaData> mediaData,
+			final Map<String, Object> properties) {
+		super(type, textContent, mediaData, properties);
 	}
 
-	public AssistantMessage(String content, Map<String, Object> properties) {
-		super(MessageType.ASSISTANT, content, properties);
+	/**
+	 * Creates a new {@link AssistantMessageBuilder} instance.
+	 * @return A new instance of AssistantMessageBuilder.
+	 */
+	public static AssistantMessageBuilder builder() {
+		return new AssistantMessageBuilder();
+	}
+
+	/**
+	 * Initializes a new {@link AssistantMessageBuilder} with settings from an existing
+	 * {@link AssistantMessage} object.
+	 * @param message The AssistantMessage object whose settings are to be used.
+	 * @return A AssistantMessageBuilder instance initialized with the provided
+	 * AssistantMessage settings.
+	 */
+	public static AssistantMessageBuilder builder(final AssistantMessage message) {
+		return builder().withContent(message.getContent()).withProperties(message.getProperties());
+	}
+
+	/**
+	 * Builder for {@link AssistantMessage}. This builder creates assistant message
+	 * object.
+	 */
+	public static class AssistantMessageBuilder extends AbstractMessageBuilder<AssistantMessageBuilder> {
+
+		private AssistantMessageBuilder() {
+			super(MessageType.ASSISTANT);
+		}
+
+		public AssistantMessageBuilder withContent(final String content) {
+			return super.withContent(content);
+		}
+
+		public AssistantMessageBuilder withProperties(final Map<String, Object> properties) {
+			return super.withProperties(properties);
+		}
+
+		public AssistantMessage build() {
+			return new AssistantMessage(this.messageType, this.textContent, this.mediaData, this.properties);
+		}
+
 	}
 
 }
