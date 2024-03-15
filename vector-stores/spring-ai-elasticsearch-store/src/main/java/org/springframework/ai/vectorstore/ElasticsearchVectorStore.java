5c2b28f6d4aec717266d4df1c46ebc97e59f54f0
Christian Tzolov <ctzolov@vmware.com>
2024-03-16 01:56:05 +0900

    Rebase, fix compilation and style errors add missing dependecies in parent pom and Bom pom

--- vector-stores/spring-ai-elasticsearch-store/src/main/java/org/springframework/ai/vectorstore/ElasticsearchVectorStore.java
+++ vector-stores/spring-ai-elasticsearch-store/src/main/java/org/springframework/ai/vectorstore/ElasticsearchVectorStore.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2023-2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.springframework.ai.vectorstore;
 
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
@@ -18,7 +34,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.ai.document.Document;
 import org.springframework.ai.embedding.EmbeddingClient;
 import org.springframework.ai.vectorstore.filter.Filter;
-import org.springframework.ai.vectorstore.filter.converter.FilterExpressionConverter;
+import org.springframework.ai.vectorstore.filter.FilterExpressionConverter;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.util.Assert;
 
@@ -29,161 +45,172 @@ import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-
+/**
+ * @author Jemin Huh
+ * @since 1.0.0
+ */
 public class ElasticsearchVectorStore implements VectorStore, InitializingBean {
 
-    // divided by 2 to get score in the range [0, 1]
-    public static final String COSINE_SIMILARITY_FUNCTION =
-            "(cosineSimilarity(params.query_vector, 'embedding') + 1.0) / 2";
+	// divided by 2 to get score in the range [0, 1]
+	public static final String COSINE_SIMILARITY_FUNCTION = "(cosineSimilarity(params.query_vector, 'embedding') + 1.0) / 2";
 
-    private static final Logger logger = LoggerFactory.getLogger(ElasticsearchVectorStore.class);
+	private static final Logger logger = LoggerFactory.getLogger(ElasticsearchVectorStore.class);
 
-    private static final String INDEX_NAME = "spring-ai-document-index";
+	private static final String INDEX_NAME = "spring-ai-document-index";
 
-    private final EmbeddingClient embeddingClient;
+	private final EmbeddingClient embeddingClient;
 
-    private final ElasticsearchClient elasticsearchClient;
+	private final ElasticsearchClient elasticsearchClient;
 
-    private final String index;
+	private final String index;
 
-    private final FilterExpressionConverter filterExpressionConverter;
+	private final FilterExpressionConverter filterExpressionConverter;
 
-    private String similarityFunction;
+	private String similarityFunction;
 
-    public ElasticsearchVectorStore(RestClient restClient, EmbeddingClient embeddingClient) {
-        this(INDEX_NAME, restClient, embeddingClient);
-    }
+	public ElasticsearchVectorStore(RestClient restClient, EmbeddingClient embeddingClient) {
+		this(INDEX_NAME, restClient, embeddingClient);
+	}
 
-    public ElasticsearchVectorStore(String index, RestClient restClient, EmbeddingClient embeddingClient) {
-        Objects.requireNonNull(embeddingClient, "RestClient must not be null");
-        Objects.requireNonNull(embeddingClient, "EmbeddingClient must not be null");
-        this.elasticsearchClient =
-                new ElasticsearchClient(new RestClientTransport(restClient, new JacksonJsonpMapper(
-                        new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false))));
-        this.embeddingClient = embeddingClient;
-        this.index = index;
-        this.filterExpressionConverter = new ElasticsearchAiSearchFilterExpressionConverter();
-        // the potential functions for vector fields at https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-script-score-query.html#vector-functions
-        this.similarityFunction = COSINE_SIMILARITY_FUNCTION;
-    }
+	public ElasticsearchVectorStore(String index, RestClient restClient, EmbeddingClient embeddingClient) {
+		Objects.requireNonNull(embeddingClient, "RestClient must not be null");
+		Objects.requireNonNull(embeddingClient, "EmbeddingClient must not be null");
+		this.elasticsearchClient = new ElasticsearchClient(new RestClientTransport(restClient, new JacksonJsonpMapper(
+				new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false))));
+		this.embeddingClient = embeddingClient;
+		this.index = index;
+		this.filterExpressionConverter = new ElasticsearchAiSearchFilterExpressionConverter();
+		// the potential functions for vector fields at
+		// https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-script-score-query.html#vector-functions
+		this.similarityFunction = COSINE_SIMILARITY_FUNCTION;
+	}
 
-    public ElasticsearchVectorStore withSimilarityFunction(String similarityFunction) {
-        this.similarityFunction = similarityFunction;
-        return this;
-    }
+	public ElasticsearchVectorStore withSimilarityFunction(String similarityFunction) {
+		this.similarityFunction = similarityFunction;
+		return this;
+	}
 
-    @Override
-    public void add(List<Document> documents) {
-        BulkRequest.Builder builkRequestBuilder = new BulkRequest.Builder();
-        for (Document document : documents) {
-            if (Objects.isNull(document.getEmbedding()) || document.getEmbedding().isEmpty()) {
-                logger.info("Calling EmbeddingClient for document id = " + document.getId());
-                document.setEmbedding(this.embeddingClient.embed(document));
-            }
-            builkRequestBuilder.operations(
-                    op -> op.index(idx -> idx.index(this.index).id(document.getId()).document(document)));
-        }
-        bulkRequest(builkRequestBuilder.build());
-    }
+	@Override
+	public void add(List<Document> documents) {
+		BulkRequest.Builder builkRequestBuilder = new BulkRequest.Builder();
+		for (Document document : documents) {
+			if (Objects.isNull(document.getEmbedding()) || document.getEmbedding().isEmpty()) {
+				logger.info("Calling EmbeddingClient for document id = " + document.getId());
+				document.setEmbedding(this.embeddingClient.embed(document));
+			}
+			builkRequestBuilder
+				.operations(op -> op.index(idx -> idx.index(this.index).id(document.getId()).document(document)));
+		}
+		bulkRequest(builkRequestBuilder.build());
+	}
 
-    @Override
-    public Optional<Boolean> delete(List<String> idList) {
-        BulkRequest.Builder builkRequestBuilder = new BulkRequest.Builder();
-        for (String id : idList)
-            builkRequestBuilder.operations(op -> op.delete(idx -> idx.index(this.index).id(id)));
-        return Optional.of(bulkRequest(builkRequestBuilder.build()).errors());
-    }
+	@Override
+	public Optional<Boolean> delete(List<String> idList) {
+		BulkRequest.Builder builkRequestBuilder = new BulkRequest.Builder();
+		for (String id : idList)
+			builkRequestBuilder.operations(op -> op.delete(idx -> idx.index(this.index).id(id)));
+		return Optional.of(bulkRequest(builkRequestBuilder.build()).errors());
+	}
 
-    private BulkResponse bulkRequest(BulkRequest bulkRequest) {
-        try {
-            return this.elasticsearchClient.bulk(bulkRequest);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
+	private BulkResponse bulkRequest(BulkRequest bulkRequest) {
+		try {
+			return this.elasticsearchClient.bulk(bulkRequest);
+		}
+		catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
 
-    @Override
-    public List<Document> similaritySearch(SearchRequest searchRequest) {
-        Assert.notNull(searchRequest, "The search request must not be null.");
-        return similaritySearch(this.embeddingClient.embed(searchRequest.getQuery()), searchRequest.getTopK(),
-                Double.valueOf(searchRequest.getSimilarityThreshold()).floatValue(),
-                searchRequest.getFilterExpression());
-    }
+	@Override
+	public List<Document> similaritySearch(SearchRequest searchRequest) {
+		Assert.notNull(searchRequest, "The search request must not be null.");
+		return similaritySearch(this.embeddingClient.embed(searchRequest.getQuery()), searchRequest.getTopK(),
+				Double.valueOf(searchRequest.getSimilarityThreshold()).floatValue(),
+				searchRequest.getFilterExpression());
+	}
 
-    public List<Document> similaritySearch(List<Double> embedding, int topK, float similarityThreshold,
-            Filter.Expression filterExpression) {
-        return similaritySearch(new co.elastic.clients.elasticsearch.core.SearchRequest.Builder().query(
-                        getElasticsearchSimilarityQuery(embedding, filterExpression)).size(topK)
-                .minScore(Float.valueOf(similarityThreshold).doubleValue()).build());
-    }
+	public List<Document> similaritySearch(List<Double> embedding, int topK, float similarityThreshold,
+			Filter.Expression filterExpression) {
+		return similaritySearch(new co.elastic.clients.elasticsearch.core.SearchRequest.Builder()
+			.query(getElasticsearchSimilarityQuery(embedding, filterExpression))
+			.size(topK)
+			.minScore(Float.valueOf(similarityThreshold).doubleValue())
+			.build());
+	}
 
-    private Query getElasticsearchSimilarityQuery(List<Double> embedding, Filter.Expression filterExpression) {
-        return Query.of(queryBuilder -> queryBuilder.scriptScore(
-                scriptScoreQueryBuilder -> scriptScoreQueryBuilder.query(
-                                queryBuilder2 -> queryBuilder2.queryString(
-                                        queryStringQuerybuilder -> queryStringQuerybuilder.query(
-                                                getElasticsearchQueryString(filterExpression))))
-                        .script(scriptBuilder -> scriptBuilder.inline(
-                                inlineScriptBuilder -> inlineScriptBuilder.source(this.similarityFunction)
-                                        .params("query_vector", JsonData.of(embedding))))));
-    }
+	private Query getElasticsearchSimilarityQuery(List<Double> embedding, Filter.Expression filterExpression) {
+		return Query.of(queryBuilder -> queryBuilder.scriptScore(scriptScoreQueryBuilder -> scriptScoreQueryBuilder
+			.query(queryBuilder2 -> queryBuilder2.queryString(queryStringQuerybuilder -> queryStringQuerybuilder
+				.query(getElasticsearchQueryString(filterExpression))))
+			.script(scriptBuilder -> scriptBuilder
+				.inline(inlineScriptBuilder -> inlineScriptBuilder.source(this.similarityFunction)
+					.params("query_vector", JsonData.of(embedding))))));
+	}
 
-    private String getElasticsearchQueryString(Filter.Expression filterExpression) {
-        return Objects.isNull(filterExpression) ? "*" : this.filterExpressionConverter.convertExpression(
-                filterExpression);
+	private String getElasticsearchQueryString(Filter.Expression filterExpression) {
+		return Objects.isNull(filterExpression) ? "*"
+				: this.filterExpressionConverter.convertExpression(filterExpression);
 
-    }
+	}
 
-    private List<Document> similaritySearch(co.elastic.clients.elasticsearch.core.SearchRequest searchRequest) {
-        try {
-            return  this.elasticsearchClient.search(searchRequest, Document.class).hits().hits().stream()
-                    .map(this::toDocument).collect(Collectors.toList());
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
+	private List<Document> similaritySearch(co.elastic.clients.elasticsearch.core.SearchRequest searchRequest) {
+		try {
+			return this.elasticsearchClient.search(searchRequest, Document.class)
+				.hits()
+				.hits()
+				.stream()
+				.map(this::toDocument)
+				.collect(Collectors.toList());
+		}
+		catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
 
-    private Document toDocument(Hit<Document> hit) {
-        Document document = hit.source();
-        document.getMetadata().put("distance", 1 - hit.score().floatValue());
-        return document;
-    }
+	private Document toDocument(Hit<Document> hit) {
+		Document document = hit.source();
+		document.getMetadata().put("distance", 1 - hit.score().floatValue());
+		return document;
+	}
 
-    public boolean exists(String targetIndex) {
-        try {
-            BooleanResponse response = this.elasticsearchClient.indices()
-                    .exists(existRequestBuilder -> existRequestBuilder.index(targetIndex));
-            return response.value();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
+	public boolean exists(String targetIndex) {
+		try {
+			BooleanResponse response = this.elasticsearchClient.indices()
+				.exists(existRequestBuilder -> existRequestBuilder.index(targetIndex));
+			return response.value();
+		}
+		catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
 
-    public CreateIndexResponse createIndexMapping(String index, String mappingJson) {
-        try {
-            return this.elasticsearchClient.indices().create(createIndexBuilder -> createIndexBuilder.index(index)
-                    .mappings(typeMappingBuilder -> typeMappingBuilder.withJson(new StringReader(mappingJson))));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
+	public CreateIndexResponse createIndexMapping(String index, String mappingJson) {
+		try {
+			return this.elasticsearchClient.indices()
+				.create(createIndexBuilder -> createIndexBuilder.index(index)
+					.mappings(typeMappingBuilder -> typeMappingBuilder.withJson(new StringReader(mappingJson))));
+		}
+		catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
 
-    @Override
-    public void afterPropertiesSet() {
-        if (!exists(this.index)) {
-            createIndexMapping(this.index, """
-                    {
-                          "properties": {
-                              "embedding": {
-                                  "type": "dense_vector",
-                                  "dims": 1536,
-                                  "index": true,
-                                  "similarity": "cosine"
-                              }
-                          }
-                      }
-                    """);
-        }
-    }
+	@Override
+	public void afterPropertiesSet() {
+		if (!exists(this.index)) {
+			createIndexMapping(this.index, """
+					{
+					      "properties": {
+					          "embedding": {
+					              "type": "dense_vector",
+					              "dims": 1536,
+					              "index": true,
+					              "similarity": "cosine"
+					          }
+					      }
+					  }
+					""");
+		}
+	}
 
 }
--- vector-stores/spring-ai-elasticsearch-store/src/test/java/org/springframework/ai/vectorstore/ElasticsearchAiSearchFilterExpressionConverterTest.java
+++ vector-stores/spring-ai-elasticsearch-store/src/test/java/org/springframework/ai/vectorstore/ElasticsearchAiSearchFilterExpressionConverterTest.java
@@ -1,8 +1,24 @@
+/*
+ * Copyright 2023-2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.springframework.ai.vectorstore;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.ai.vectorstore.filter.Filter;
-import org.springframework.ai.vectorstore.filter.converter.FilterExpressionConverter;
+import org.springframework.ai.vectorstore.filter.FilterExpressionConverter;
 
 import java.util.Date;
 import java.util.List;
@@ -11,99 +27,92 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.ai.vectorstore.filter.Filter.ExpressionType.*;
 
 class ElasticsearchAiSearchFilterExpressionConverterTest {
-    final FilterExpressionConverter converter = new ElasticsearchAiSearchFilterExpressionConverter();
 
-    @Test
-    public void testDate() {
-        String vectorExpr = converter
-                .convertExpression(
-                        new Filter.Expression(EQ, new Filter.Key("activationDate"),
-                                new Filter.Value(new Date(1704637752148L))));
-        assertThat(vectorExpr).isEqualTo("metadata.activationDate:2024-01-07T14:29:12Z");
+	final FilterExpressionConverter converter = new ElasticsearchAiSearchFilterExpressionConverter();
 
-        vectorExpr = converter
-                .convertExpression(new Filter.Expression(EQ, new Filter.Key("activationDate"),
-                        new Filter.Value("1970-01-01T00:00:02Z")));
-        assertThat(vectorExpr).isEqualTo("metadata.activationDate:1970-01-01T00:00:02Z");
-    }
+	@Test
+	public void testDate() {
+		String vectorExpr = converter.convertExpression(new Filter.Expression(EQ, new Filter.Key("activationDate"),
+				new Filter.Value(new Date(1704637752148L))));
+		assertThat(vectorExpr).isEqualTo("metadata.activationDate:2024-01-07T14:29:12Z");
 
-    @Test
-    public void testEQ() {
-        String vectorExpr = converter.convertExpression(
-                new Filter.Expression(EQ, new Filter.Key("country"), new Filter.Value("BG")));
-        assertThat(vectorExpr).isEqualTo("metadata.country:BG");
-    }
+		vectorExpr = converter.convertExpression(
+				new Filter.Expression(EQ, new Filter.Key("activationDate"), new Filter.Value("1970-01-01T00:00:02Z")));
+		assertThat(vectorExpr).isEqualTo("metadata.activationDate:1970-01-01T00:00:02Z");
+	}
 
-    @Test
-    public void tesEqAndGte() {
-        String vectorExpr = converter
-                .convertExpression(new Filter.Expression(AND,
-                        new Filter.Expression(EQ, new Filter.Key("genre"), new Filter.Value("drama")),
-                        new Filter.Expression(GTE, new Filter.Key("year"), new Filter.Value(2020))));
-        assertThat(vectorExpr).isEqualTo("metadata.genre:drama AND metadata.year:>=2020");
-    }
+	@Test
+	public void testEQ() {
+		String vectorExpr = converter
+			.convertExpression(new Filter.Expression(EQ, new Filter.Key("country"), new Filter.Value("BG")));
+		assertThat(vectorExpr).isEqualTo("metadata.country:BG");
+	}
 
-    @Test
-    public void tesIn() {
-        String vectorExpr = converter.convertExpression(
-                new Filter.Expression(IN, new Filter.Key("genre"),
-                        new Filter.Value(List.of("comedy", "documentary", "drama"))));
-        assertThat(vectorExpr).isEqualTo("(metadata.genre:comedy OR documentary OR drama)");
-    }
+	@Test
+	public void tesEqAndGte() {
+		String vectorExpr = converter.convertExpression(new Filter.Expression(AND,
+				new Filter.Expression(EQ, new Filter.Key("genre"), new Filter.Value("drama")),
+				new Filter.Expression(GTE, new Filter.Key("year"), new Filter.Value(2020))));
+		assertThat(vectorExpr).isEqualTo("metadata.genre:drama AND metadata.year:>=2020");
+	}
 
-    @Test
-    public void testNe() {
-        String vectorExpr = converter
-                .convertExpression(new Filter.Expression(OR,
-                        new Filter.Expression(GTE, new Filter.Key("year"), new Filter.Value(2020)),
-                        new Filter.Expression(AND,
-                                new Filter.Expression(EQ, new Filter.Key("country"), new Filter.Value("BG")),
-                                new Filter.Expression(NE, new Filter.Key("city"), new Filter.Value("Sofia")))));
-        assertThat(vectorExpr).isEqualTo("metadata.year:>=2020 OR metadata.country:BG AND metadata.city: NOT Sofia");
-    }
+	@Test
+	public void tesIn() {
+		String vectorExpr = converter.convertExpression(new Filter.Expression(IN, new Filter.Key("genre"),
+				new Filter.Value(List.of("comedy", "documentary", "drama"))));
+		assertThat(vectorExpr).isEqualTo("(metadata.genre:comedy OR documentary OR drama)");
+	}
 
-    @Test
-    public void testGroup() {
-        String vectorExpr = converter.convertExpression(new Filter.Expression(AND,
-                new Filter.Group(new Filter.Expression(OR,
-                        new Filter.Expression(GTE, new Filter.Key("year"), new Filter.Value(2020)),
-                        new Filter.Expression(EQ, new Filter.Key("country"), new Filter.Value("BG")))),
-                new Filter.Expression(NIN, new Filter.Key("city"), new Filter.Value(List.of("Sofia", "Plovdiv")))));
-        assertThat(vectorExpr)
-                .isEqualTo("(metadata.year:>=2020 OR metadata.country:BG) AND NOT (metadata.city:Sofia OR Plovdiv)");
-    }
+	@Test
+	public void testNe() {
+		String vectorExpr = converter.convertExpression(
+				new Filter.Expression(OR, new Filter.Expression(GTE, new Filter.Key("year"), new Filter.Value(2020)),
+						new Filter.Expression(AND,
+								new Filter.Expression(EQ, new Filter.Key("country"), new Filter.Value("BG")),
+								new Filter.Expression(NE, new Filter.Key("city"), new Filter.Value("Sofia")))));
+		assertThat(vectorExpr).isEqualTo("metadata.year:>=2020 OR metadata.country:BG AND metadata.city: NOT Sofia");
+	}
 
-    @Test
-    public void tesBoolean() {
-        String vectorExpr = converter.convertExpression(new Filter.Expression(AND,
-                new Filter.Expression(AND, new Filter.Expression(EQ, new Filter.Key("isOpen"), new Filter.Value(true)),
-                        new Filter.Expression(GTE, new Filter.Key("year"), new Filter.Value(2020))),
-                new Filter.Expression(IN, new Filter.Key("country"), new Filter.Value(List.of("BG", "NL", "US")))));
+	@Test
+	public void testGroup() {
+		String vectorExpr = converter.convertExpression(new Filter.Expression(AND,
+				new Filter.Group(new Filter.Expression(OR,
+						new Filter.Expression(GTE, new Filter.Key("year"), new Filter.Value(2020)),
+						new Filter.Expression(EQ, new Filter.Key("country"), new Filter.Value("BG")))),
+				new Filter.Expression(NIN, new Filter.Key("city"), new Filter.Value(List.of("Sofia", "Plovdiv")))));
+		assertThat(vectorExpr)
+			.isEqualTo("(metadata.year:>=2020 OR metadata.country:BG) AND NOT (metadata.city:Sofia OR Plovdiv)");
+	}
 
-        assertThat(vectorExpr).isEqualTo(
-                "metadata.isOpen:true AND metadata.year:>=2020 AND (metadata.country:BG OR NL OR US)");
-    }
+	@Test
+	public void tesBoolean() {
+		String vectorExpr = converter.convertExpression(new Filter.Expression(AND,
+				new Filter.Expression(AND, new Filter.Expression(EQ, new Filter.Key("isOpen"), new Filter.Value(true)),
+						new Filter.Expression(GTE, new Filter.Key("year"), new Filter.Value(2020))),
+				new Filter.Expression(IN, new Filter.Key("country"), new Filter.Value(List.of("BG", "NL", "US")))));
 
-    @Test
-    public void testDecimal() {
-        String vectorExpr = converter
-                .convertExpression(new Filter.Expression(AND,
-                        new Filter.Expression(GTE, new Filter.Key("temperature"), new Filter.Value(-15.6)),
-                        new Filter.Expression(LTE, new Filter.Key("temperature"), new Filter.Value(20.13))));
+		assertThat(vectorExpr)
+			.isEqualTo("metadata.isOpen:true AND metadata.year:>=2020 AND (metadata.country:BG OR NL OR US)");
+	}
 
-        assertThat(vectorExpr).isEqualTo("metadata.temperature:>=-15.6 AND metadata.temperature:<=20.13");
-    }
+	@Test
+	public void testDecimal() {
+		String vectorExpr = converter.convertExpression(new Filter.Expression(AND,
+				new Filter.Expression(GTE, new Filter.Key("temperature"), new Filter.Value(-15.6)),
+				new Filter.Expression(LTE, new Filter.Key("temperature"), new Filter.Value(20.13))));
 
-    @Test
-    public void testComplexIdentifiers() {
-        String vectorExpr = converter
-                .convertExpression(
-                        new Filter.Expression(EQ, new Filter.Key("\"country 1 2 3\""), new Filter.Value("BG")));
-        assertThat(vectorExpr).isEqualTo("metadata.country 1 2 3:BG");
+		assertThat(vectorExpr).isEqualTo("metadata.temperature:>=-15.6 AND metadata.temperature:<=20.13");
+	}
 
-        vectorExpr = converter.convertExpression(
-                new Filter.Expression(EQ, new Filter.Key("'country 1 2 3'"), new Filter.Value("BG")));
-        assertThat(vectorExpr).isEqualTo("metadata.country 1 2 3:BG");
-    }
+	@Test
+	public void testComplexIdentifiers() {
+		String vectorExpr = converter
+			.convertExpression(new Filter.Expression(EQ, new Filter.Key("\"country 1 2 3\""), new Filter.Value("BG")));
+		assertThat(vectorExpr).isEqualTo("metadata.country 1 2 3:BG");
+
+		vectorExpr = converter
+			.convertExpression(new Filter.Expression(EQ, new Filter.Key("'country 1 2 3'"), new Filter.Value("BG")));
+		assertThat(vectorExpr).isEqualTo("metadata.country 1 2 3:BG");
+	}
 
 }
