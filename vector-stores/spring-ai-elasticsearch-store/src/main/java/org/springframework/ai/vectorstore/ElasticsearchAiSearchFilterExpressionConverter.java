b38552c517b2f1b971aa877779b6b1f39910ba2c
Christian Tzolov <ctzolov@vmware.com>
2024-03-16 01:56:05 +0900

    Rebase, fix compilation and style errors add missing dependecies in parent pom and Bom pom

--- vector-stores/spring-ai-elasticsearch-store/src/main/java/org/springframework/ai/vectorstore/ElasticsearchAiSearchFilterExpressionConverter.java
+++ vector-stores/spring-ai-elasticsearch-store/src/main/java/org/springframework/ai/vectorstore/ElasticsearchAiSearchFilterExpressionConverter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023-2023 the original author or authors.
+ * Copyright 2023-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -28,114 +28,124 @@ import java.util.List;
 import java.util.TimeZone;
 import java.util.regex.Pattern;
 
+/**
+ * @author Jemin Huh
+ * @since 1.0.0
+ */
 public class ElasticsearchAiSearchFilterExpressionConverter extends AbstractFilterExpressionConverter {
 
-    private static final Pattern DATE_FORMAT_PATTERN = Pattern.compile("\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}Z");
+	private static final Pattern DATE_FORMAT_PATTERN = Pattern.compile("\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}Z");
 
-    private final SimpleDateFormat dateFormat;
+	private final SimpleDateFormat dateFormat;
 
-    public ElasticsearchAiSearchFilterExpressionConverter() {
-        this.dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
-        this.dateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
-    }
+	public ElasticsearchAiSearchFilterExpressionConverter() {
+		this.dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+		this.dateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+	}
 
-    @Override
-    protected void doExpression(Expression expression, StringBuilder context) {
-        if (expression.type() == Filter.ExpressionType.IN || expression.type() == Filter.ExpressionType.NIN) {
-            context.append(getOperationSymbol(expression));
-            context.append("(");
-            this.convertOperand(expression.left(), context);
-            this.convertOperand(expression.right(), context);
-            context.append(")");
-        } else {
-            this.convertOperand(expression.left(), context);
-            context.append(getOperationSymbol(expression));
-            this.convertOperand(expression.right(), context);
-        }
-    }
+	@Override
+	protected void doExpression(Expression expression, StringBuilder context) {
+		if (expression.type() == Filter.ExpressionType.IN || expression.type() == Filter.ExpressionType.NIN) {
+			context.append(getOperationSymbol(expression));
+			context.append("(");
+			this.convertOperand(expression.left(), context);
+			this.convertOperand(expression.right(), context);
+			context.append(")");
+		}
+		else {
+			this.convertOperand(expression.left(), context);
+			context.append(getOperationSymbol(expression));
+			this.convertOperand(expression.right(), context);
+		}
+	}
 
-    @Override
-    protected void doStartValueRange(Filter.Value listValue, StringBuilder context) {
-    }
+	@Override
+	protected void doStartValueRange(Filter.Value listValue, StringBuilder context) {
+	}
 
-    @Override
-    protected void doEndValueRange(Filter.Value listValue, StringBuilder context) {
-    }
+	@Override
+	protected void doEndValueRange(Filter.Value listValue, StringBuilder context) {
+	}
 
-    @Override
-    protected void doAddValueRangeSpitter(Filter.Value listValue, StringBuilder context) {
-        context.append(" OR ");
-    }
+	@Override
+	protected void doAddValueRangeSpitter(Filter.Value listValue, StringBuilder context) {
+		context.append(" OR ");
+	}
 
-    private String getOperationSymbol(Expression exp) {
-        return switch (exp.type()) {
-            case AND -> " AND ";
-            case OR -> " OR ";
-            case EQ, IN -> "";
-            case NE -> " NOT ";
-            case LT -> "<";
-            case LTE -> "<=";
-            case GT -> ">";
-            case GTE -> ">=";
-            case NIN -> "NOT ";
-            default -> throw new RuntimeException("Not supported expression type: " + exp.type());
-        };
-    }
+	private String getOperationSymbol(Expression exp) {
+		return switch (exp.type()) {
+			case AND -> " AND ";
+			case OR -> " OR ";
+			case EQ, IN -> "";
+			case NE -> " NOT ";
+			case LT -> "<";
+			case LTE -> "<=";
+			case GT -> ">";
+			case GTE -> ">=";
+			case NIN -> "NOT ";
+			default -> throw new RuntimeException("Not supported expression type: " + exp.type());
+		};
+	}
 
-    @Override
-    public void doKey(Key key, StringBuilder context) {
-        var identifier = hasOuterQuotes(key.key()) ? removeOuterQuotes(key.key()) : key.key();
-        var prefixedIdentifier = withMetaPrefix(identifier);
-        context.append(prefixedIdentifier.trim()).append(":");
-    }
+	@Override
+	public void doKey(Key key, StringBuilder context) {
+		var identifier = hasOuterQuotes(key.key()) ? removeOuterQuotes(key.key()) : key.key();
+		var prefixedIdentifier = withMetaPrefix(identifier);
+		context.append(prefixedIdentifier.trim()).append(":");
+	}
 
-    public String withMetaPrefix(String identifier) {
-        return "metadata." + identifier;
-    }
+	public String withMetaPrefix(String identifier) {
+		return "metadata." + identifier;
+	}
 
-    @Override
-    protected void doValue(Filter.Value filterValue, StringBuilder context) {
-        if (filterValue.value() instanceof List list) {
-            int c = 0;
-            for (Object v : list) {
-                context.append(v);
-                if (c++ < list.size() - 1) {
-                    this.doAddValueRangeSpitter(filterValue, context);
-                }
-            }
-        } else {
-            this.doSingleValue(filterValue.value(), context);
-        }
-    }
+	@Override
+	protected void doValue(Filter.Value filterValue, StringBuilder context) {
+		if (filterValue.value() instanceof List list) {
+			int c = 0;
+			for (Object v : list) {
+				context.append(v);
+				if (c++ < list.size() - 1) {
+					this.doAddValueRangeSpitter(filterValue, context);
+				}
+			}
+		}
+		else {
+			this.doSingleValue(filterValue.value(), context);
+		}
+	}
 
-    @Override
-    protected void doSingleValue(Object value, StringBuilder context) {
-        if (value instanceof Date date) {
-            context.append(this.dateFormat.format(date));
-        } else if (value instanceof String text) {
-            if (DATE_FORMAT_PATTERN.matcher(text).matches()) {
-                try {
-                    Date date = this.dateFormat.parse(text);
-                    context.append(this.dateFormat.format(date));
-                } catch (ParseException e) {
-                    throw new IllegalArgumentException("Invalid date type:" + text, e);
-                }
-            } else {
-                context.append(text);
-            }
-        } else {
-            context.append(value);
-        }
-    }
+	@Override
+	protected void doSingleValue(Object value, StringBuilder context) {
+		if (value instanceof Date date) {
+			context.append(this.dateFormat.format(date));
+		}
+		else if (value instanceof String text) {
+			if (DATE_FORMAT_PATTERN.matcher(text).matches()) {
+				try {
+					Date date = this.dateFormat.parse(text);
+					context.append(this.dateFormat.format(date));
+				}
+				catch (ParseException e) {
+					throw new IllegalArgumentException("Invalid date type:" + text, e);
+				}
+			}
+			else {
+				context.append(text);
+			}
+		}
+		else {
+			context.append(value);
+		}
+	}
 
-    @Override
-    public void doStartGroup(Filter.Group group, StringBuilder context) {
-        context.append("(");
-    }
+	@Override
+	public void doStartGroup(Filter.Group group, StringBuilder context) {
+		context.append("(");
+	}
 
-    @Override
-    public void doEndGroup(Filter.Group group, StringBuilder context) {
-        context.append(")");
-    }
+	@Override
+	public void doEndGroup(Filter.Group group, StringBuilder context) {
+		context.append(")");
+	}
 
 }
