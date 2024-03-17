1ca729ae0352ca4b474d5a6aeff1dd2fa1450a5b
Eddú Meléndez <eddu.melendez@gmail.com>
2024-03-16 22:22:48 -0600

    Add MongoDB Atlas vector store auto-configuration
    It also provides a starter.

---
+++ spring-ai-spring-boot-autoconfigure/src/main/java/org/springframework/ai/autoconfigure/vectorstore/mongo/MongoDBAtlasVectorStoreProperties.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2023 - 2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.ai.autoconfigure.vectorstore.mongo;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * @author Eddú Meléndez
+ */
+@ConfigurationProperties("spring.ai.vectorstore.mongodb")
+public class MongoDBAtlasVectorStoreProperties {
+
+	private String collectionName;
+
+	private String pathName;
+
+	private String indexName;
+
+	public String getCollectionName() {
+		return this.collectionName;
+	}
+
+	public void setCollectionName(String collectionName) {
+		this.collectionName = collectionName;
+	}
+
+	public String getPathName() {
+		return this.pathName;
+	}
+
+	public void setPathName(String pathName) {
+		this.pathName = pathName;
+	}
+
+	public String getIndexName() {
+		return this.indexName;
+	}
+
+	public void setIndexName(String indexName) {
+		this.indexName = indexName;
+	}
+
+}
