ae447b9900b8623168ac6e8ccc1aaf6eb3cd76ac
PabloSanchi <pablo.sanchi.herrera@gmail.com>
2024-04-02 13:23:41 +0200

    fix: check the model is included in request parameters

--- models/spring-ai-watsonx-ai/src/main/java/org/springframework/ai/watsonx/api/WatsonxAiRequest.java
+++ models/spring-ai-watsonx-ai/src/main/java/org/springframework/ai/watsonx/api/WatsonxAiRequest.java
@@ -21,6 +21,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import org.springframework.ai.watsonx.WatsonxAiChatOptions;
+import org.springframework.util.Assert;
 
 // @formatter:off
 @JsonInclude(JsonInclude.Include.NON_NULL)
@@ -71,6 +72,7 @@ public class WatsonxAiRequest {
         }
 
         public Builder withParameters(Map<String, Object> parameters) {
+            Assert.notNull(parameters.get("model"), "Model parameter is required");
             this.model = parameters.get("model").toString();
             this.parameters = WatsonxAiChatOptions.filterNonSupportedFields(parameters);
             return this;
