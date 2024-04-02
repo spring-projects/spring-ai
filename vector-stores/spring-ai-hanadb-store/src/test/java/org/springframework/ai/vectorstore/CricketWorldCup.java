62cccbf637466025a91878122c18bda11a90c04b
Rahul <rahulmitt@yahoo.com>
2024-04-02 14:54:55 +0530

    Adding SAP HanaDB as a vector store

---
+++ vector-stores/spring-ai-hanadb-store/src/test/java/org/springframework/ai/vectorstore/CricketWorldCup.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2024 - 2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.ai.vectorstore;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import lombok.extern.jackson.Jacksonized;
+
+/**
+ * @author Rahul Mittal
+ * @since 1.0.0
+ */
+@Entity
+@Table(name = "CRICKET_WORLD_CUP")
+@Data
+@Jacksonized
+@SuperBuilder(toBuilder = true)
+@NoArgsConstructor
+public class CricketWorldCup extends HanaVectorEntity {
+    @Column(name = "content")
+    private String content;
+}
+
