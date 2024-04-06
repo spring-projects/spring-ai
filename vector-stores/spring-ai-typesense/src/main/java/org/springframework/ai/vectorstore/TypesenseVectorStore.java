bb7bee984b887ea778ee383eea9540212244d893
PabloSanchi <pablo.sanchi.herrera@gmail.com>
2024-04-06 19:06:35 +0200

    feat: typesense vectorstore setup

---
+++ vector-stores/spring-ai-typesense/src/main/java/org/springframework/ai/vectorstore/TypesenseVectorStore.java
@@ -0,0 +1,23 @@
+package org.springframework.ai.vectorstore;
+
+import org.springframework.ai.document.Document;
+
+import java.util.List;
+import java.util.Optional;
+
+public class TypesenseVectorStore implements VectorStore {
+    @Override
+    public void add(List<Document> documents) {
+
+    }
+
+    @Override
+    public Optional<Boolean> delete(List<String> idList) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Document> similaritySearch(SearchRequest request) {
+        return null;
+    }
+}
