3ef748ca8d7f6478457d23e96d709de752ba604b
GR <gr@fastball.dev>
2024-04-23 09:26:32 +0800

    feat: Add ZhiPu AI model client

---
+++ spring-ai-spring-boot-autoconfigure/src/test/java/org/springframework/ai/autoconfigure/zhipuai/tool/FunctionCallbackWithPlainFunctionBeanIT.java
@@ -0,0 +1,172 @@
+/*
+ * Copyright 2023 - 2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.ai.autoconfigure.zhipuai.tool;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.ai.autoconfigure.retry.SpringAiRetryAutoConfiguration;
+import org.springframework.ai.autoconfigure.zhipuai.ZhiPuAiAutoConfiguration;
+import org.springframework.ai.chat.ChatResponse;
+import org.springframework.ai.chat.Generation;
+import org.springframework.ai.chat.messages.AssistantMessage;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.model.function.FunctionCallingOptions;
+import org.springframework.ai.model.function.FunctionCallingOptionsBuilder.PortableFunctionCallingOptions;
+import org.springframework.ai.zhipuai.ZhiPuAiChatClient;
+import org.springframework.ai.zhipuai.ZhiPuAiChatOptions;
+import org.springframework.boot.autoconfigure.AutoConfigurations;
+import org.springframework.boot.autoconfigure.web.client.RestClientAutoConfiguration;
+import org.springframework.boot.test.context.runner.ApplicationContextRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Description;
+import reactor.core.publisher.Flux;
+
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author Geng Rong
+ */
+@EnabledIfEnvironmentVariable(named = "ZHIPU_AI_API_KEY", matches = ".*")
+class FunctionCallbackWithPlainFunctionBeanIT {
+
+	private final Logger logger = LoggerFactory.getLogger(FunctionCallbackWithPlainFunctionBeanIT.class);
+
+	private final ApplicationContextRunner contextRunner = new ApplicationContextRunner()
+		.withPropertyValues("spring.ai.zhipuai.apiKey=" + System.getenv("ZHIPU_AI_API_KEY"))
+		.withConfiguration(AutoConfigurations.of(SpringAiRetryAutoConfiguration.class,
+				RestClientAutoConfiguration.class, ZhiPuAiAutoConfiguration.class))
+		.withUserConfiguration(Config.class);
+
+	@Test
+	void functionCallTest() {
+		contextRunner.withPropertyValues("spring.ai.zhipuai.chat.options.model=glm-4").run(context -> {
+
+			ZhiPuAiChatClient chatClient = context.getBean(ZhiPuAiChatClient.class);
+
+			// Test weatherFunction
+			UserMessage userMessage = new UserMessage("What's the weather like in San Francisco, Tokyo, and Paris?");
+
+			ChatResponse response = chatClient.call(new Prompt(List.of(userMessage),
+					ZhiPuAiChatOptions.builder().withFunction("weatherFunction").build()));
+
+			logger.info("Response: {}", response);
+
+			assertThat(response.getResult().getOutput().getContent()).contains("30", "10", "15");
+
+			// Test weatherFunctionTwo
+			response = chatClient.call(new Prompt(List.of(userMessage),
+					ZhiPuAiChatOptions.builder().withFunction("weatherFunctionTwo").build()));
+
+			logger.info("Response: {}", response);
+
+			assertThat(response.getResult().getOutput().getContent()).contains("30", "10", "15");
+
+		});
+	}
+
+	@Test
+	void functionCallWithPortableFunctionCallingOptions() {
+		contextRunner.withPropertyValues("spring.ai.zhipuai.chat.options.model=glm-4").run(context -> {
+
+			ZhiPuAiChatClient chatClient = context.getBean(ZhiPuAiChatClient.class);
+
+			// Test weatherFunction
+			UserMessage userMessage = new UserMessage("What's the weather like in San Francisco, Tokyo, and Paris?");
+
+			PortableFunctionCallingOptions functionOptions = FunctionCallingOptions.builder()
+				.withFunction("weatherFunction")
+				.build();
+
+			ChatResponse response = chatClient.call(new Prompt(List.of(userMessage), functionOptions));
+
+			logger.info("Response: {}", response);
+		});
+	}
+
+	@Test
+	void streamFunctionCallTest() {
+		contextRunner.withPropertyValues("spring.ai.zhipuai.chat.options.model=glm-4").run(context -> {
+
+			ZhiPuAiChatClient chatClient = context.getBean(ZhiPuAiChatClient.class);
+
+			// Test weatherFunction
+			UserMessage userMessage = new UserMessage("What's the weather like in San Francisco, Tokyo, and Paris?");
+
+			Flux<ChatResponse> response = chatClient.stream(new Prompt(List.of(userMessage),
+					ZhiPuAiChatOptions.builder().withFunction("weatherFunction").build()));
+
+			String content = response.collectList()
+				.block()
+				.stream()
+				.map(ChatResponse::getResults)
+				.flatMap(List::stream)
+				.map(Generation::getOutput)
+				.map(AssistantMessage::getContent)
+				.collect(Collectors.joining());
+			logger.info("Response: {}", content);
+
+			assertThat(content).containsAnyOf("30.0", "30");
+			assertThat(content).containsAnyOf("10.0", "10");
+			assertThat(content).containsAnyOf("15.0", "15");
+
+			// Test weatherFunctionTwo
+			response = chatClient.stream(new Prompt(List.of(userMessage),
+					ZhiPuAiChatOptions.builder().withFunction("weatherFunctionTwo").build()));
+
+			content = response.collectList()
+				.block()
+				.stream()
+				.map(ChatResponse::getResults)
+				.flatMap(List::stream)
+				.map(Generation::getOutput)
+				.map(AssistantMessage::getContent)
+				.collect(Collectors.joining());
+			logger.info("Response: {}", content);
+
+			assertThat(content).containsAnyOf("30.0", "30");
+			assertThat(content).containsAnyOf("10.0", "10");
+			assertThat(content).containsAnyOf("15.0", "15");
+		});
+	}
+
+	@Configuration
+	static class Config {
+
+		@Bean
+		@Description("Get the weather in location")
+		public Function<MockWeatherService.Request, MockWeatherService.Response> weatherFunction() {
+			return new MockWeatherService();
+		}
+
+		// Relies on the Request's JsonClassDescription annotation to provide the
+		// function description.
+		@Bean
+		public Function<MockWeatherService.Request, MockWeatherService.Response> weatherFunctionTwo() {
+			MockWeatherService weatherService = new MockWeatherService();
+			return (weatherService::apply);
+		}
+
+	}
+
+}
