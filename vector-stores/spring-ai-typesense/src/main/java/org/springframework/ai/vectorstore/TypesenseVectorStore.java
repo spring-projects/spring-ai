13348c97493fdcf9f3bb0f5959925aebdbf82c20
PabloSanchi <pablo.sanchi.herrera@gmail.com>
2024-05-16 17:26:54 +0200

    fix: remove unused import

--- vector-stores/spring-ai-typesense/src/main/java/org/springframework/ai/vectorstore/TypesenseVectorStore.java
+++ vector-stores/spring-ai-typesense/src/main/java/org/springframework/ai/vectorstore/TypesenseVectorStore.java
@@ -1,6 +1,5 @@
 package org.springframework.ai.vectorstore;
 
-import com.fasterxml.jackson.databind.util.JSONPObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.ai.document.Document;
