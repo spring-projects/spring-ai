30fb170bb370f8036ab903dac16c663aedcfe08f
zhangqian <zhangqian1@chinagoods.com>
2024-05-28 16:08:03 +0800

    Rename the bean name, which is different from the naming of spring data redis redisConnectionDetails

--- spring-ai-spring-boot-autoconfigure/src/main/java/org/springframework/ai/autoconfigure/vectorstore/redis/RedisVectorStoreAutoConfiguration.java
+++ spring-ai-spring-boot-autoconfigure/src/main/java/org/springframework/ai/autoconfigure/vectorstore/redis/RedisVectorStoreAutoConfiguration.java
@@ -35,7 +35,7 @@ public class RedisVectorStoreAutoConfiguration {
 
 	@Bean
 	@ConditionalOnMissingBean(RedisConnectionDetails.class)
-	public PropertiesRedisConnectionDetails redisConnectionDetails(RedisVectorStoreProperties properties) {
+	public PropertiesRedisConnectionDetails redisVectorStoreConnectionDetails(RedisVectorStoreProperties properties) {
 		return new PropertiesRedisConnectionDetails(properties);
 	}
 
