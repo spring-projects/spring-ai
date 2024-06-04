06ff651734bf70dc2fcec63db9f13d4ccaa178eb
Piotr Olaszewski <piotr.olaszewski@thulium.pl>
2024-06-04 07:51:00 +0200

    Remove duplicated ApiUtils and polish javadocs

---
+++ models/spring-ai-zhipuai/src/main/java/org/springframework/ai/zhipuai/api/ZhiPuApiConstants.java
@@ -0,0 +1,13 @@
+package org.springframework.ai.zhipuai.api;
+
+/**
+ * Common value constants for ZhiPu api.
+ *
+ * @author Piotr Olaszewski
+ * @since 1.0.0 M2
+ */
+public final class ZhiPuApiConstants {
+
+	public static final String DEFAULT_BASE_URL = "https://open.bigmodel.cn/api/paas";
+
+}
