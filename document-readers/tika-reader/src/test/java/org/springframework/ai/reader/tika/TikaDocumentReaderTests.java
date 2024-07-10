eeaf150cdf0030698550e8bf8a6a607122d79189
andrea.rubino <andrea.rubino@welld.ch>
2024-07-10 12:16:45 +0200

    Add support for custom metadata for TikaDocumentReader

--- document-readers/tika-reader/src/test/java/org/springframework/ai/reader/tika/TikaDocumentReaderTests.java
+++ document-readers/tika-reader/src/test/java/org/springframework/ai/reader/tika/TikaDocumentReaderTests.java
@@ -19,6 +19,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * @author Christian Tzolov
@@ -43,6 +44,30 @@ public class TikaDocumentReaderTests {
 		assertThat(doc.getMetadata()).containsKeys(TikaDocumentReader.METADATA_SOURCE);
 		assertThat(doc.getMetadata().get(TikaDocumentReader.METADATA_SOURCE)).isEqualTo(resourceName);
 		assertThat(doc.getContent()).contains(contentSnipped);
+	}
+
+	@ParameterizedTest
+	@CsvSource({
+			"classpath:/word-sample.docx,word-sample.docx,Two kinds of links are possible, those that refer to an external website",
+			"classpath:/word-sample.doc,word-sample.doc,The limited permissions granted above are perpetual and will not be revoked by OASIS",
+			"classpath:/sample2.pdf,sample2.pdf,Consult doc/pdftex/manual.pdf from your tetex distribution for more",
+			"classpath:/sample.ppt,sample.ppt,Sed ipsum tortor, fringilla a consectetur eget, cursus posuere sem.",
+			"classpath:/sample.pptx,sample.pptx,Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+			"https://github.com/spring-projects/spring-ai/,https://github.com/spring-projects/spring-ai/,An Application Framework for AI Engineering" })
+	public void testCustomMetadata(String resourceUri, String resourceName, String contentSnipped) {
+
+		var reader = new TikaDocumentReader(resourceUri);
+		reader.getCustomMetadata().put("CUSTOM_METADATA_KEY", "CUSTOM_METADATA_VALUE");
+
+		var docs = reader.get();
+
+		assertThat(docs).hasSize(1);
+
+		var doc = docs.get(0);
+
+		assertThat(doc.getMetadata()).containsKeys(TikaDocumentReader.METADATA_SOURCE);
+		assertThat(doc.getMetadata().get(TikaDocumentReader.METADATA_SOURCE)).isEqualTo(resourceName);
+		assertEquals("CUSTOM_METADATA_VALUE", doc.getMetadata().get("CUSTOM_METADATA_KEY"));
 	}
 
 }
