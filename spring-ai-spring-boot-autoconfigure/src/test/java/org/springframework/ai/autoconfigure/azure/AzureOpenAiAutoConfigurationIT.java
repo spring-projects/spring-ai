053aaf81e9eaec67ed443ec7d5a82b0044c7e0a2
Piotr Olaszewski <piotr.olaszewski@thulium.pl>
2024-07-22 07:45:31 +0200

    Add test to AzureOpenAiAutoConfiguration

--- spring-ai-spring-boot-autoconfigure/src/test/java/org/springframework/ai/autoconfigure/azure/AzureOpenAiAutoConfigurationIT.java
+++ spring-ai-spring-boot-autoconfigure/src/test/java/org/springframework/ai/autoconfigure/azure/AzureOpenAiAutoConfigurationIT.java
@@ -15,32 +15,35 @@
  */
 package org.springframework.ai.autoconfigure.azure;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
+import org.springframework.ai.autoconfigure.azure.openai.AzureOpenAiAutoConfiguration;
+import org.springframework.ai.azure.openai.AzureOpenAiAudioTranscriptionModel;
+import org.springframework.ai.azure.openai.AzureOpenAiChatModel;
+import org.springframework.ai.azure.openai.AzureOpenAiEmbeddingModel;
+import org.springframework.ai.chat.messages.AssistantMessage;
+import org.springframework.ai.chat.messages.Message;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.model.Generation;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.SystemPromptTemplate;
+import org.springframework.ai.embedding.EmbeddingResponse;
+import org.springframework.boot.autoconfigure.AutoConfigurations;
+import org.springframework.boot.test.context.runner.ApplicationContextRunner;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import reactor.core.publisher.Flux;
+
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
-import org.springframework.ai.azure.openai.AzureOpenAiChatModel;
-import org.springframework.ai.chat.messages.AssistantMessage;
-import reactor.core.publisher.Flux;
-
-import org.springframework.ai.autoconfigure.azure.openai.AzureOpenAiAutoConfiguration;
-import org.springframework.ai.azure.openai.AzureOpenAiEmbeddingModel;
-import org.springframework.ai.chat.model.ChatResponse;
-import org.springframework.ai.chat.model.Generation;
-import org.springframework.ai.embedding.EmbeddingResponse;
-import org.springframework.ai.chat.prompt.Prompt;
-import org.springframework.ai.chat.prompt.SystemPromptTemplate;
-import org.springframework.ai.chat.messages.Message;
-import org.springframework.ai.chat.messages.UserMessage;
-import org.springframework.boot.autoconfigure.AutoConfigurations;
-import org.springframework.boot.test.context.runner.ApplicationContextRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * @author Christian Tzolov
+ * @author Piotr Olaszewski
  * @since 0.8.0
  */
 @EnabledIfEnvironmentVariable(named = "AZURE_OPENAI_API_KEY", matches = ".+")
@@ -52,7 +55,7 @@ public class AzureOpenAiAutoConfigurationIT {
 	private static String EMBEDDING_MODEL_NAME = "text-embedding-ada-002";
 
 	private final ApplicationContextRunner contextRunner = new ApplicationContextRunner().withPropertyValues(
-	// @formatter:off
+			// @formatter:off
 			"spring.ai.azure.openai.api-key=" + System.getenv("AZURE_OPENAI_API_KEY"),
 			"spring.ai.azure.openai.endpoint=" + System.getenv("AZURE_OPENAI_ENDPOINT"),
 
@@ -95,11 +98,11 @@ public class AzureOpenAiAutoConfigurationIT {
 			assertThat(responses.size()).isGreaterThan(10);
 
 			String stitchedResponseContent = responses.stream()
-				.map(ChatResponse::getResults)
-				.flatMap(List::stream)
-				.map(Generation::getOutput)
-				.map(AssistantMessage::getContent)
-				.collect(Collectors.joining());
+					.map(ChatResponse::getResults)
+					.flatMap(List::stream)
+					.map(Generation::getOutput)
+					.map(AssistantMessage::getContent)
+					.collect(Collectors.joining());
 
 			assertThat(stitchedResponseContent).contains("Blackbeard");
 		});
@@ -111,7 +114,7 @@ public class AzureOpenAiAutoConfigurationIT {
 			AzureOpenAiEmbeddingModel embeddingModel = context.getBean(AzureOpenAiEmbeddingModel.class);
 
 			EmbeddingResponse embeddingResponse = embeddingModel
-				.embedForResponse(List.of("Hello World", "World is big and salvation is near"));
+					.embedForResponse(List.of("Hello World", "World is big and salvation is near"));
 			assertThat(embeddingResponse.getResults()).hasSize(2);
 			assertThat(embeddingResponse.getResults().get(0).getOutput()).isNotEmpty();
 			assertThat(embeddingResponse.getResults().get(0).getIndex()).isEqualTo(0);
@@ -157,6 +160,17 @@ public class AzureOpenAiAutoConfigurationIT {
 		// Explicitly enable the embedding auto-configuration.
 		contextRunner.withPropertyValues("spring.ai.azure.openai.embedding.enabled=true").run(context -> {
 			assertThat(context.getBeansOfType(AzureOpenAiEmbeddingModel.class)).isNotEmpty();
+		});
+	}
+
+	@Test
+	void transcribe() {
+		contextRunner.run(context -> {
+			AzureOpenAiAudioTranscriptionModel transcriptionModel = context
+					.getBean(AzureOpenAiAudioTranscriptionModel.class);
+			Resource audioFile = new ClassPathResource("/speech/jfk.flac");
+			String response = transcriptionModel.call(audioFile);
+			assertThat(response).isEqualTo("And so my fellow Americans, ask not what your country can do for you, ask what you can do for your country.");
 		});
 	}
 
