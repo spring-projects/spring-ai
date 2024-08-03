05c33d8041f3ae46609cd59b27701f6e3a403cda
andrew.han <andrew.han@imile.me>
2024-08-03 18:20:59 +0800

    fork

---
+++ spring-ai-core/src/test/java/org/springframework/ai/HanSpingAi.java
@@ -0,0 +1,13 @@
+package org.springframework.ai;
+
+/**
+ * @author hanjiabao
+ * @since 2024/6/6
+ */
+public class HanSpingAi {
+
+    public static void main(String[] args) {
+        System.out.printf("Hello, Spring AI!");
+    }
+
+}
