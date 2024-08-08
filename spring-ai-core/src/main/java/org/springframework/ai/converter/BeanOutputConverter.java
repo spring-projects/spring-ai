91afed5ae5baf36a56b96dc2a4f31805a2f4e05e
Christian Tzolov <ctzolov@vmware.com>
2024-08-08 18:47:53 +0200

    OpenAi: Add support for structured outputs and JSON schema
    - Added support for OpenAI's structured outputs feature, which allows specifying a JSON schema for the model to match
    - Introduced new record to configure the desired response format
    - Added support for configuring the response format via application properties or the chat options builder
    - Extend teh BeanOutputConverter to help generate JSON schema from a target domain object and convert the response.
    - Added comprehensive tests to cover the new response format functionality
    
    Resolves #1196

--- spring-ai-core/src/main/java/org/springframework/ai/converter/BeanOutputConverter.java
+++ spring-ai-core/src/main/java/org/springframework/ai/converter/BeanOutputConverter.java
@@ -35,10 +35,12 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.github.victools.jsonschema.generator.Option;
 import com.github.victools.jsonschema.generator.SchemaGenerator;
 import com.github.victools.jsonschema.generator.SchemaGeneratorConfig;
 import com.github.victools.jsonschema.generator.SchemaGeneratorConfigBuilder;
 import com.github.victools.jsonschema.module.jackson.JacksonModule;
+import com.github.victools.jsonschema.module.jackson.JacksonOption;
 
 /**
  * An implementation of {@link StructuredOutputConverter} that transforms the LLM output
@@ -140,9 +142,10 @@ public class BeanOutputConverter<T> implements StructuredOutputConverter<T> {
 	 * Generates the JSON schema for the target type.
 	 */
 	private void generateSchema() {
-		JacksonModule jacksonModule = new JacksonModule();
+		JacksonModule jacksonModule = new JacksonModule(JacksonOption.RESPECT_JSONPROPERTY_REQUIRED);
 		SchemaGeneratorConfigBuilder configBuilder = new SchemaGeneratorConfigBuilder(DRAFT_2020_12, PLAIN_JSON)
-			.with(jacksonModule);
+			.with(jacksonModule)
+			.with(Option.FORBIDDEN_ADDITIONAL_PROPERTIES_BY_DEFAULT);
 		SchemaGeneratorConfig config = configBuilder.build();
 		SchemaGenerator generator = new SchemaGenerator(config);
 		JsonNode jsonNode = generator.generateSchema(this.typeRef.getType());
@@ -203,6 +206,14 @@ public class BeanOutputConverter<T> implements StructuredOutputConverter<T> {
 				```%s```
 				""";
 		return String.format(template, this.jsonSchema);
+	}
+
+	/**
+	 * Provides the generated JSON schema for the target type.
+	 * @return The generated JSON schema.
+	 */
+	public String getJsonSchema() {
+		return this.jsonSchema;
 	}
 
 }
