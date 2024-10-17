d244bec658e8be900194f29a4c3184a0e70be27f
Theo van Kraay <theo.van@microsoft.com>
2024-10-17 21:33:29 +0100

    address review comments

--- spring-ai-spring-boot-autoconfigure/src/main/java/org/springframework/ai/autoconfigure/vectorstore/cosmosdb/CosmosDBVectorStoreProperties.java
+++ spring-ai-spring-boot-autoconfigure/src/main/java/org/springframework/ai/autoconfigure/vectorstore/cosmosdb/CosmosDBVectorStoreProperties.java
@@ -1,7 +1,28 @@
+/*
+ * Copyright 2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.springframework.ai.autoconfigure.vectorstore.cosmosdb;
 
 import org.springframework.ai.autoconfigure.vectorstore.CommonVectorStoreProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * @author Theo van Kraay
+ * @since 1.0.0
+ */
 
 @ConfigurationProperties(CosmosDBVectorStoreProperties.CONFIG_PREFIX)
 public class CosmosDBVectorStoreProperties extends CommonVectorStoreProperties {
@@ -87,4 +108,5 @@ public class CosmosDBVectorStoreProperties extends CommonVectorStoreProperties {
 	public void setVectorDimensions(long vectorDimensions) {
 		this.vectorDimensions = vectorDimensions;
 	}
+
 }
--- vector-stores/spring-ai-azure-cosmos-db-store/src/main/java/org/springframework/ai/vectorstore/CosmosDBFilterExpressionConverter.java
+++ vector-stores/spring-ai-azure-cosmos-db-store/src/main/java/org/springframework/ai/vectorstore/CosmosDBFilterExpressionConverter.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package org.springframework.ai.vectorstore;
 
@@ -18,6 +33,9 @@ import static org.springframework.ai.vectorstore.filter.Filter.ExpressionType.OR
 /**
  * Converts {@link org.springframework.ai.vectorstore.filter.Filter.Expression} into
  * Cosmos DB NoSQL API where clauses.
+ *
+ * @author Theo van Kraay
+ * @since 1.0.0
  */
 class CosmosDBFilterExpressionConverter extends AbstractFilterExpressionConverter {
 
@@ -120,4 +138,5 @@ class CosmosDBFilterExpressionConverter extends AbstractFilterExpressionConverte
 				throw new RuntimeException("Not supported expression type:" + exp.type());
 		}
 	}
+
 }
