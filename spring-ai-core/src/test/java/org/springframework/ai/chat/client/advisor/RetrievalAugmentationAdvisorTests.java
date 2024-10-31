bc50d28f2db64e8738a7f8b7470f4c053e6de56e
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2024-10-31 21:30:00 +0100

    Modular RAG: Query Augmentor
    * Add new QueryAugmentor API, a component for augmenting a user query with contextual data.
    * Implement ContextualQueryAugmentor that combines the content of each document and add it to the original user prompt, with support for the scenario where the context is empty.
    * Extend RetrievalAugmentationAdvisor to use the new augmentation building block.
    * Introduce utility to assist in validating arguments for prompt-related operations.
    
    Relates to gh-#1603
    
    Signed-off-by: Thomas Vitale <ThomasVitale@users.noreply.github.com>

--- spring-ai-core/src/test/java/org/springframework/ai/chat/client/advisor/RetrievalAugmentationAdvisorTests.java
+++ spring-ai-core/src/test/java/org/springframework/ai/chat/client/advisor/RetrievalAugmentationAdvisorTests.java
@@ -94,18 +94,23 @@ class RetrievalAugmentationAdvisorTests {
 
 		var prompt = promptCaptor.getValue();
 		assertThat(prompt.getContents()).contains("""
-				What would I get if I added a pinch of Moonstone to a dash of powdered Gold?
-
-				Context information is below. Use this information to answer the user query.
+				Context information is below.
 
 				---------------------
 				doc1
 				doc2
 				---------------------
 
-				Given the context and provided history information and not prior knowledge,
-				reply to the user query. If the answer is not in the context, inform
-				the user that you can't answer the query.
+				Given the context information and no prior knowledge, answer the query.
+
+				Follow these rules:
+
+				1. If the answer is not in the context, just say that you don't know.
+				2. Avoid statements like "Based on the context..." or "The provided information...".
+
+				Query: What would I get if I added a pinch of Moonstone to a dash of powdered Gold?
+
+				Answer:
 				""");
 	}
 
