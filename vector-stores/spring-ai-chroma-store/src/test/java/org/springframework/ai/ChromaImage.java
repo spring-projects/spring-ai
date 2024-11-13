af0179f49ab6f1cd6d82aace5601ef0a0422ed32
Ilayaperumal Gopinathan <ilayaperumal.gopinathan@broadcom.com>
2024-11-13 07:20:11 +0000

    GH-1529 Fix ChromaVectorStore delete Ids operation
     - As a result of the the change:https://github.com/chroma-core/chroma/pull/2880  introduced in chroma:0.5.13,the delete operation doesn't return any values. Hence, we need to check the Http Client Response with the status code instead of return value.
       - Check the status code for successful delete operation
       - Update test
    
    Resolves #1529

--- vector-stores/spring-ai-chroma-store/src/test/java/org/springframework/ai/ChromaImage.java
+++ vector-stores/spring-ai-chroma-store/src/test/java/org/springframework/ai/ChromaImage.java
@@ -23,7 +23,7 @@ import org.testcontainers.utility.DockerImageName;
  */
 public final class ChromaImage {
 
-	public static final DockerImageName DEFAULT_IMAGE = DockerImageName.parse("ghcr.io/chroma-core/chroma:0.5.11");
+	public static final DockerImageName DEFAULT_IMAGE = DockerImageName.parse("ghcr.io/chroma-core/chroma:0.5.16");
 
 	private ChromaImage() {
 
