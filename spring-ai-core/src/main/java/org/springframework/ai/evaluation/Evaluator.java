85580f81fa0d03103baad4699c9932162d17de1b
Mark Pollack <mark.pollack@broadcom.com>
2024-12-12 09:22:04 -0500

    Fix RAG tests after Document refactoring

--- spring-ai-core/src/main/java/org/springframework/ai/evaluation/Evaluator.java
+++ spring-ai-core/src/main/java/org/springframework/ai/evaluation/Evaluator.java
@@ -19,6 +19,7 @@ package org.springframework.ai.evaluation;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.ai.document.Document;
 import org.springframework.ai.model.Content;
 import org.springframework.util.StringUtils;
 
@@ -28,9 +29,9 @@ public interface Evaluator {
 	EvaluationResponse evaluate(EvaluationRequest evaluationRequest);
 
 	default String doGetSupportingData(EvaluationRequest evaluationRequest) {
-		List<Content> data = evaluationRequest.getDataList();
+		List<Document> data = evaluationRequest.getDataList();
 		return data.stream()
-			.map(Content::getText)
+			.map(Document::getText)
 			.filter(StringUtils::hasText)
 			.collect(Collectors.joining(System.lineSeparator()));
 	}
