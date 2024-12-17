627fb79e5e24e8f95892c49a54f00afc33e4f7ea
Ilayaperumal Gopinathan <ilayaperumal.gopinathan@broadcom.com>
2024-12-17 01:45:32 +0000

    Refactor ImageOptions builder method
     - Deprecate the builder method with the prefix 'with' and add new methods

--- models/spring-ai-azure-openai/src/test/java/org/springframework/ai/azure/openai/image/AzureOpenAiImageModelIT.java
+++ models/spring-ai-azure-openai/src/test/java/org/springframework/ai/azure/openai/image/AzureOpenAiImageModelIT.java
@@ -53,7 +53,7 @@ public class AzureOpenAiImageModelIT {
 
 	@Test
 	void imageAsUrlTest() {
-		var options = ImageOptionsBuilder.builder().withHeight(1024).withWidth(1024).build();
+		var options = ImageOptionsBuilder.builder().height(1024).width(1024).build();
 
 		var instructions = """
 				A light cream colored mini golden doodle with a sign that contains the message "I'm on my way to BARCADE!".""";
