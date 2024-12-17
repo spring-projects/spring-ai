7a7a914dc0adaac80cd766921eed85d13df55a05
Soby Chacko <soby.chacko@broadcom.com>
2024-12-17 15:52:18 -0500

    Add builder pattern to CoherenceVectorStore and refactor package name

--- vector-stores/spring-ai-coherence-store/src/test/java/org/springframework/ai/vectorstore/coherence/CoherenceFilterExpressionConverterTests.java
+++ vector-stores/spring-ai-coherence-store/src/test/java/org/springframework/ai/vectorstore/coherence/CoherenceFilterExpressionConverterTests.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.springframework.ai.vectorstore;
+package org.springframework.ai.vectorstore.coherence;
 
 import com.tangosol.util.Filters;
 import com.tangosol.util.ValueExtractor;
