c5bf6440393eb277e358915c889f8b9102fd43ee
Christian Tzolov <ctzolov@vmware.com>
2024-12-20 15:09:05 +0100

    fix(mistral) Fix the PaymentStatusFunctionCallingIT configuraiton
    Resolves #1853

--- models/spring-ai-mistral-ai/src/test/java/org/springframework/ai/mistralai/api/tool/PaymentStatusFunctionCallingIT.java
+++ models/spring-ai-mistral-ai/src/test/java/org/springframework/ai/mistralai/api/tool/PaymentStatusFunctionCallingIT.java
@@ -53,7 +53,7 @@ import static org.assertj.core.api.Assertions.assertThat;
  * @author Christian Tzolov
  * @since 0.8.1
  */
-@Disabled("See https://github.com/spring-projects/spring-ai/issues/1853")
+// @Disabled("See https://github.com/spring-projects/spring-ai/issues/1853")
 @EnabledIfEnvironmentVariable(named = "MISTRAL_AI_API_KEY", matches = ".+")
 public class PaymentStatusFunctionCallingIT {
 
@@ -131,7 +131,7 @@ public class PaymentStatusFunctionCallingIT {
 
 			// Extend conversation with function response.
 			// The functionName is used to identify the function response!
-			messages.add(new ChatCompletionMessage(result.toString(), Role.TOOL, functionName, null));
+			messages.add(new ChatCompletionMessage(result.toString(), Role.TOOL, functionName, null, toolCall.id()));
 		}
 
 		response = mistralApi
