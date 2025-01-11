e7bf32deee3023efe38b11e63ae8cb985265cc65
Enrico <enrico.rampazzo@live.com>
2025-01-11 23:35:43 +0400

    removed Cassandra leftover Signed-off-by: Enrico Rampazzo <enrico.rampazzo@live.com>

--- spring-ai-spring-boot-autoconfigure/src/test/java/org/springframework/ai/autoconfigure/chat/memory/neo4j/Neo4jChatMemoryPropertiesTest.java
+++ spring-ai-spring-boot-autoconfigure/src/test/java/org/springframework/ai/autoconfigure/chat/memory/neo4j/Neo4jChatMemoryPropertiesTest.java
@@ -17,8 +17,6 @@
 package org.springframework.ai.autoconfigure.chat.memory.neo4j;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.ai.autoconfigure.chat.memory.cassandra.CassandraChatMemoryProperties;
-import org.springframework.ai.chat.memory.cassandra.CassandraChatMemoryConfig;
 import org.springframework.ai.chat.memory.neo4j.Neo4jChatMemoryConfig;
 
 import java.time.Duration;
@@ -41,23 +39,4 @@ class Neo4jChatMemoryPropertiesTest {
 		assertThat(props.getToolCallLabel()).isEqualTo(Neo4jChatMemoryConfig.DEFAULT_TOOL_CALL_LABEL);
 		assertThat(props.getToolResponseLabel()).isEqualTo(Neo4jChatMemoryConfig.DEFAULT_TOOL_RESPONSE_LABEL);
 	}
-
-	@Test
-	void customValues() {
-		var props = new CassandraChatMemoryProperties();
-		props.setKeyspace("my_keyspace");
-		props.setTable("my_table");
-		props.setAssistantColumn("my_assistant_column");
-		props.setUserColumn("my_user_column");
-		props.setTimeToLive(Duration.ofDays(1));
-		props.setInitializeSchema(false);
-
-		assertThat(props.getKeyspace()).isEqualTo("my_keyspace");
-		assertThat(props.getTable()).isEqualTo("my_table");
-		assertThat(props.getAssistantColumn()).isEqualTo("my_assistant_column");
-		assertThat(props.getUserColumn()).isEqualTo("my_user_column");
-		assertThat(props.getTimeToLive()).isEqualTo(Duration.ofDays(1));
-		assertThat(props.isInitializeSchema()).isFalse();
-	}
-
 }
