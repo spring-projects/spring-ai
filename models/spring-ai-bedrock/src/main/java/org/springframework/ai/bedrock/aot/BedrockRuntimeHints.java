ca27895da40578bb4c85954c105355ae5e551cf7
Ilayaperumal Gopinathan <ilayaperumal.gopinathan@broadcom.com>
2025-01-29 09:43:52 +0000

    Remove deprecated Amazon Bedrock chat models
     - Amzon Bedrock chat models were deprecated to support Amazon Bedrock Converse API for the chat models.
     - This PR removes all the references of the deprecated Amazon Bedrock chat models
       - Remove Amazon Bedrock chat models for anthropic, anthropic3, cohere, jurassic2, titan
          - Remove API, chat options and model
          - Remove tests and doc references
       - Update the doc to reflect the changes
       - Update upgrade notes
    
    Resolves #2124
    
    Signed-off-by: Ilayaperumal Gopinathan <ilayaperumal.gopinathan@broadcom.com>

--- models/spring-ai-bedrock/src/main/java/org/springframework/ai/bedrock/aot/BedrockRuntimeHints.java
+++ models/spring-ai-bedrock/src/main/java/org/springframework/ai/bedrock/aot/BedrockRuntimeHints.java
@@ -16,21 +16,10 @@
 
 package org.springframework.ai.bedrock.aot;
 
-import org.springframework.ai.bedrock.anthropic.AnthropicChatOptions;
-import org.springframework.ai.bedrock.anthropic.api.AnthropicChatBedrockApi;
-import org.springframework.ai.bedrock.anthropic3.Anthropic3ChatOptions;
-import org.springframework.ai.bedrock.anthropic3.api.Anthropic3ChatBedrockApi;
 import org.springframework.ai.bedrock.api.AbstractBedrockApi;
-import org.springframework.ai.bedrock.cohere.BedrockCohereChatOptions;
 import org.springframework.ai.bedrock.cohere.BedrockCohereEmbeddingOptions;
-import org.springframework.ai.bedrock.cohere.api.CohereChatBedrockApi;
 import org.springframework.ai.bedrock.cohere.api.CohereEmbeddingBedrockApi;
-import org.springframework.ai.bedrock.jurassic2.api.Ai21Jurassic2ChatBedrockApi;
-import org.springframework.ai.bedrock.llama.BedrockLlamaChatOptions;
-import org.springframework.ai.bedrock.llama.api.LlamaChatBedrockApi;
-import org.springframework.ai.bedrock.titan.BedrockTitanChatOptions;
 import org.springframework.ai.bedrock.titan.BedrockTitanEmbeddingOptions;
-import org.springframework.ai.bedrock.titan.api.TitanChatBedrockApi;
 import org.springframework.ai.bedrock.titan.api.TitanEmbeddingBedrockApi;
 import org.springframework.aot.hint.MemberCategory;
 import org.springframework.aot.hint.RuntimeHints;
@@ -55,16 +44,7 @@ public class BedrockRuntimeHints implements RuntimeHintsRegistrar {
 		for (var tr : findJsonAnnotatedClassesInPackage(AbstractBedrockApi.class)) {
 			hints.reflection().registerType(tr, mcs);
 		}
-		for (var tr : findJsonAnnotatedClassesInPackage(Ai21Jurassic2ChatBedrockApi.class)) {
-			hints.reflection().registerType(tr, mcs);
-		}
 
-		for (var tr : findJsonAnnotatedClassesInPackage(CohereChatBedrockApi.class)) {
-			hints.reflection().registerType(tr, mcs);
-		}
-		for (var tr : findJsonAnnotatedClassesInPackage(BedrockCohereChatOptions.class)) {
-			hints.reflection().registerType(tr, mcs);
-		}
 		for (var tr : findJsonAnnotatedClassesInPackage(CohereEmbeddingBedrockApi.class)) {
 			hints.reflection().registerType(tr, mcs);
 		}
@@ -72,37 +52,10 @@ public class BedrockRuntimeHints implements RuntimeHintsRegistrar {
 			hints.reflection().registerType(tr, mcs);
 		}
 
-		for (var tr : findJsonAnnotatedClassesInPackage(LlamaChatBedrockApi.class)) {
-			hints.reflection().registerType(tr, mcs);
-		}
-		for (var tr : findJsonAnnotatedClassesInPackage(BedrockLlamaChatOptions.class)) {
-			hints.reflection().registerType(tr, mcs);
-		}
-
-		for (var tr : findJsonAnnotatedClassesInPackage(TitanChatBedrockApi.class)) {
-			hints.reflection().registerType(tr, mcs);
-		}
-		for (var tr : findJsonAnnotatedClassesInPackage(BedrockTitanChatOptions.class)) {
-			hints.reflection().registerType(tr, mcs);
-		}
 		for (var tr : findJsonAnnotatedClassesInPackage(BedrockTitanEmbeddingOptions.class)) {
 			hints.reflection().registerType(tr, mcs);
 		}
 		for (var tr : findJsonAnnotatedClassesInPackage(TitanEmbeddingBedrockApi.class)) {
-			hints.reflection().registerType(tr, mcs);
-		}
-
-		for (var tr : findJsonAnnotatedClassesInPackage(AnthropicChatBedrockApi.class)) {
-			hints.reflection().registerType(tr, mcs);
-		}
-		for (var tr : findJsonAnnotatedClassesInPackage(AnthropicChatOptions.class)) {
-			hints.reflection().registerType(tr, mcs);
-		}
-
-		for (var tr : findJsonAnnotatedClassesInPackage(Anthropic3ChatBedrockApi.class)) {
-			hints.reflection().registerType(tr, mcs);
-		}
-		for (var tr : findJsonAnnotatedClassesInPackage(Anthropic3ChatOptions.class)) {
 			hints.reflection().registerType(tr, mcs);
 		}
 	}
