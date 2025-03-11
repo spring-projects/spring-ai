d5203ed038e808d8d29d654552cc2e8e4c2c875f
Soby Chacko <soby.chacko@broadcom.com>
2025-03-11 18:09:46 -0400

    Fix array comparison and add final modifiers in embedding classes
    - Fix Embedding equals() using Arrays.equals() for embedding array comparison
    - Fix Embedding hashCode() using Arrays.hashCode() for proper array hashing
    - Add final modifiers to fields in Embedding classes
    
    Signed-off-by: Soby Chacko <soby.chacko@broadcom.com>

--- spring-ai-core/src/main/java/org/springframework/ai/embedding/AbstractEmbeddingModel.java
+++ spring-ai-core/src/main/java/org/springframework/ai/embedding/AbstractEmbeddingModel.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023-2024 the original author or authors.
+ * Copyright 2023-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -32,7 +32,7 @@ import org.springframework.core.io.DefaultResourceLoader;
  */
 public abstract class AbstractEmbeddingModel implements EmbeddingModel {
 
-	private static Map<String, Integer> KNOWN_EMBEDDING_DIMENSIONS = loadKnownModelDimensions();
+	private static final Map<String, Integer> KNOWN_EMBEDDING_DIMENSIONS = loadKnownModelDimensions();
 
 	/**
 	 * Default constructor.
