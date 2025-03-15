bfed6bbe8964156458811ea6504de9fc3850511d
ghdcksgml1 <ghdcksgml2@naver.com>
2025-03-15 11:44:13 +0900

    Add java docs
    Signed-off-by: ghdcksgml1 <ghdcksgml2@naver.com>
    (cherry picked from commit cb11592659b7ca2e535f9162538e94951400b99a)
    Signed-off-by: ghdcksgml1 <ghdcksgml2@naver.com>

--- spring-ai-core/src/main/java/org/springframework/ai/chat/client/advisor/api/AdvisedResponseStreamUtils.java
+++ spring-ai-core/src/main/java/org/springframework/ai/chat/client/advisor/api/AdvisedResponseStreamUtils.java
@@ -5,8 +5,19 @@ import org.springframework.util.StringUtils;
 
 import java.util.function.Predicate;
 
+/**
+ * A stream utility class to provide support methods handling {@link AdvisedResponse}.
+ */
 public final class AdvisedResponseStreamUtils {
 
+	/**
+	 * Returns a predicate that checks whether the provided {@link AdvisedResponse}
+	 * contains a {@link ChatResponse} with at least one result having a non-empty
+	 * finish reason in its metadata.
+	 *
+	 * @return a {@link Predicate} that evaluates whether the finish reason exists
+	 *         within the response metadata.
+	 */
 	public static Predicate<AdvisedResponse> onFinishReason() {
 		return advisedResponse -> {
 			ChatResponse chatResponse = advisedResponse.response();
