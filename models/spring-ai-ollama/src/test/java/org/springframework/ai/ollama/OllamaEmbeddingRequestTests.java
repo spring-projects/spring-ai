3d1825dd059a304c264af9d87801adfcb3b2b958
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2025-03-19 11:02:37 -0700

    ollama: Adopt new strategy for ObservationContext
    Relates to gh-2518
    
    Signed-off-by: Thomas Vitale <ThomasVitale@users.noreply.github.com>

--- models/spring-ai-ollama/src/test/java/org/springframework/ai/ollama/OllamaEmbeddingRequestTests.java
+++ models/spring-ai-ollama/src/test/java/org/springframework/ai/ollama/OllamaEmbeddingRequestTests.java
@@ -21,6 +21,7 @@ import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
+import org.springframework.ai.embedding.EmbeddingRequest;
 import org.springframework.ai.ollama.api.OllamaApi;
 import org.springframework.ai.ollama.api.OllamaOptions;
 
@@ -40,19 +41,18 @@ public class OllamaEmbeddingRequestTests {
 
 	@Test
 	public void ollamaEmbeddingRequestDefaultOptions() {
+		var embeddingRequest = this.embeddingModel.buildEmbeddingRequest(new EmbeddingRequest(List.of("Hello"), null));
+		var ollamaRequest = this.embeddingModel.ollamaEmbeddingRequest(embeddingRequest);
 
-		var request = this.embeddingModel.ollamaEmbeddingRequest(List.of("Hello"), null);
-
-		assertThat(request.model()).isEqualTo("DEFAULT_MODEL");
-		assertThat(request.options().get("num_gpu")).isEqualTo(1);
-		assertThat(request.options().get("main_gpu")).isEqualTo(11);
-		assertThat(request.options().get("use_mmap")).isEqualTo(true);
-		assertThat(request.input()).isEqualTo(List.of("Hello"));
+		assertThat(ollamaRequest.model()).isEqualTo("DEFAULT_MODEL");
+		assertThat(ollamaRequest.options().get("num_gpu")).isEqualTo(1);
+		assertThat(ollamaRequest.options().get("main_gpu")).isEqualTo(11);
+		assertThat(ollamaRequest.options().get("use_mmap")).isEqualTo(true);
+		assertThat(ollamaRequest.input()).isEqualTo(List.of("Hello"));
 	}
 
 	@Test
 	public void ollamaEmbeddingRequestRequestOptions() {
-
 		var promptOptions = OllamaOptions.builder()//
 			.model("PROMPT_MODEL")//
 			.mainGPU(22)//
@@ -60,23 +60,26 @@ public class OllamaEmbeddingRequestTests {
 			.numGPU(2)
 			.build();
 
-		var request = this.embeddingModel.ollamaEmbeddingRequest(List.of("Hello"), promptOptions);
+		var embeddingRequest = this.embeddingModel
+			.buildEmbeddingRequest(new EmbeddingRequest(List.of("Hello"), promptOptions));
+		var ollamaRequest = this.embeddingModel.ollamaEmbeddingRequest(embeddingRequest);
 
-		assertThat(request.model()).isEqualTo("PROMPT_MODEL");
-		assertThat(request.options().get("num_gpu")).isEqualTo(2);
-		assertThat(request.options().get("main_gpu")).isEqualTo(22);
-		assertThat(request.options().get("use_mmap")).isEqualTo(true);
-		assertThat(request.input()).isEqualTo(List.of("Hello"));
+		assertThat(ollamaRequest.model()).isEqualTo("PROMPT_MODEL");
+		assertThat(ollamaRequest.options().get("num_gpu")).isEqualTo(2);
+		assertThat(ollamaRequest.options().get("main_gpu")).isEqualTo(22);
+		assertThat(ollamaRequest.options().get("use_mmap")).isEqualTo(true);
+		assertThat(ollamaRequest.input()).isEqualTo(List.of("Hello"));
 	}
 
 	@Test
 	public void ollamaEmbeddingRequestWithNegativeKeepAlive() {
-
 		var promptOptions = OllamaOptions.builder().model("PROMPT_MODEL").keepAlive("-1m").build();
 
-		var request = this.embeddingModel.ollamaEmbeddingRequest(List.of("Hello"), promptOptions);
+		var embeddingRequest = this.embeddingModel
+			.buildEmbeddingRequest(new EmbeddingRequest(List.of("Hello"), promptOptions));
+		var ollamaRequest = this.embeddingModel.ollamaEmbeddingRequest(embeddingRequest);
 
-		assertThat(request.keepAlive()).isEqualTo(Duration.ofMinutes(-1));
+		assertThat(ollamaRequest.keepAlive()).isEqualTo(Duration.ofMinutes(-1));
 	}
 
 }
