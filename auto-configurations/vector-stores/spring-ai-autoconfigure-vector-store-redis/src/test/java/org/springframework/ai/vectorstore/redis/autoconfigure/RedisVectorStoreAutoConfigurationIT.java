d94a3c9f40fc869d8a67d4b373051e7aaf60dc5e
Soby Chacko <soby.chacko@broadcom.com>
2025-04-01 17:13:41 -0400

    Moving LoggingMarkers and ResourceUtils from spring-ai-client-chat to spring-ai-commons
    Signed-off-by: Soby Chacko <soby.chacko@broadcom.com>

--- auto-configurations/vector-stores/spring-ai-autoconfigure-vector-store-redis/src/test/java/org/springframework/ai/vectorstore/redis/autoconfigure/RedisVectorStoreAutoConfigurationIT.java
+++ auto-configurations/vector-stores/spring-ai-autoconfigure-vector-store-redis/src/test/java/org/springframework/ai/vectorstore/redis/autoconfigure/RedisVectorStoreAutoConfigurationIT.java
@@ -25,7 +25,7 @@ import org.junit.jupiter.api.Test;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
-import org.springframework.ai.ResourceUtils;
+import org.springframework.ai.util.ResourceUtils;
 import org.springframework.ai.document.Document;
 import org.springframework.ai.embedding.EmbeddingModel;
 import org.springframework.ai.observation.conventions.VectorStoreProvider;
--- spring-ai-commons/src/main/java/org/springframework/ai/util/ResourceUtils.java
+++ spring-ai-commons/src/main/java/org/springframework/ai/util/ResourceUtils.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.springframework.ai;
+package org.springframework.ai.util;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
