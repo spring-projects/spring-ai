a0392564afdc483fbbeaa2a4a3375b362fab65b2
Łukasz Jernaś <lukasz.jernas@allegro.com>
2025-04-09 14:43:05 +0200

    Create MongoDB chat memory implementation
    Signed-off-by: Łukasz Jernaś <lukasz.jernas@allegro.com>

---
+++ auto-configurations/models/chat/memory/spring-ai-autoconfigure-model-chat-memory-mongodb/src/main/java/org/springframework/ai/model/chat/memory/mongodb/autoconfigure/MongoDbChatMemoryIndexCreator.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2025-2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.ai.model.chat.memory.mongodb.autoconfigure;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.ai.chat.memory.mongodb.Conversation;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.index.Index;
+import org.springframework.stereotype.Component;
+
+/**
+ * Class responsible for creating MongoDB proper indices for the ChatMemory. Creates a
+ * main index on the conversationId and timestamp fields, and a TTL index on the timestamp
+ * field if the TTL is set in properties.
+ *
+ * @see MongoDbChatMemoryProperties
+ * @author Łukasz Jernaś
+ * @since 1.0.0
+ */
+@Component
+@ConditionalOnProperty(value = "spring.ai.chat.memory.mongodb.create-indexes", havingValue = "true")
+public class MongoDbChatMemoryIndexCreator {
+
+	private static final Logger logger = LoggerFactory.getLogger(MongoDbChatMemoryIndexCreator.class);
+
+	private final MongoTemplate mongoTemplate;
+
+	private final MongoDbChatMemoryProperties mongoDbChatMemoryProperties;
+
+	public MongoDbChatMemoryIndexCreator(MongoTemplate mongoTemplate,
+			MongoDbChatMemoryProperties mongoDbChatMemoryProperties) {
+		this.mongoTemplate = mongoTemplate;
+		this.mongoDbChatMemoryProperties = mongoDbChatMemoryProperties;
+	}
+
+	@EventListener(ContextRefreshedEvent.class)
+	public void initIndicesAfterStartup() {
+		logger.info("Creating MongoDB indices for ChatMemory");
+		// Create a main index
+		mongoTemplate.indexOps(Conversation.class)
+			.ensureIndex(new Index().on("conversationId", Sort.Direction.ASC).on("timestamp", Sort.Direction.DESC));
+
+		createOrUpdateTtlIndex();
+
+	}
+
+	private void createOrUpdateTtlIndex() {
+		if (!this.mongoDbChatMemoryProperties.getTtl().isZero()) {
+			// Check for existing TTL index
+			mongoTemplate.indexOps(Conversation.class).getIndexInfo().forEach(idx -> {
+				logger.error("Index info: {}", idx);
+				if (idx.getExpireAfter().isPresent()
+						&& !idx.getExpireAfter().get().equals(this.mongoDbChatMemoryProperties.getTtl())) {
+					logger.warn("Dropping existing TTL index, because TTL is different");
+					mongoTemplate.indexOps(Conversation.class).dropIndex(idx.getName());
+				}
+			});
+			mongoTemplate.indexOps(Conversation.class)
+				.ensureIndex(new Index().on("timestamp", Sort.Direction.ASC)
+					.expire(this.mongoDbChatMemoryProperties.getTtl()));
+		}
+	}
+
+}
---
+++ memory/spring-ai-model-chat-memory-mongodb/src/test/java/org/springframework/ai/chat/memory/mongodb/MongoDbChatMemoryTestIT.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright 2024-2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.ai.chat.memory.mongodb;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.messages.AssistantMessage;
+import org.springframework.ai.chat.messages.MessageType;
+import org.springframework.ai.chat.messages.SystemMessage;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.context.runner.ApplicationContextRunner;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@Testcontainers
+class MongoDbChatMemoryTestIT {
+
+	@Container
+	@ServiceConnection
+	static MongoDBContainer mongoDbContainer = new MongoDBContainer("mongo");
+
+	private final ApplicationContextRunner contextRunner = new ApplicationContextRunner()
+		.withUserConfiguration(MongoDbChatMemoryTestIT.TestApplication.class)
+		.withPropertyValues(
+				String.format("spring.data.mongodb.uri=%s/ai_test", mongoDbContainer.getConnectionString()));
+
+	@Test
+	void ensureBeanGetsCreated() {
+		this.contextRunner.run(context -> {
+			MongoDbChatMemory memory = context.getBean(MongoDbChatMemory.class);
+			Assertions.assertNotNull(memory);
+		});
+	}
+
+	@ParameterizedTest
+	@CsvSource({ "Message from assistant,ASSISTANT", "Message from user,USER", "Message from system,SYSTEM" })
+	void add_shouldInsertSingleMessage(String content, MessageType messageType) {
+		this.contextRunner.run(context -> {
+			var chatMemory = context.getBean(ChatMemory.class);
+			var conversationId = UUID.randomUUID().toString();
+			var message = switch (messageType) {
+				case ASSISTANT -> new AssistantMessage(content + " - " + conversationId);
+				case USER -> new UserMessage(content + " - " + conversationId);
+				case SYSTEM -> new SystemMessage(content + " - " + conversationId);
+				default -> throw new IllegalArgumentException("Type not supported: " + messageType);
+			};
+			chatMemory.add(conversationId, message);
+			var messages = chatMemory.get(conversationId, 1);
+			assertEquals(1, messages.size());
+			assertEquals(message, messages.get(0));
+		});
+	}
+
+	@SpringBootConfiguration
+	@EnableAutoConfiguration
+	public static class TestApplication {
+
+		@Bean
+		public ChatMemory chatMemory(MongoTemplate mongoTemplate) {
+			var config = MongoDbChatMemoryConfig.builder().withTemplate(mongoTemplate).build();
+			return MongoDbChatMemory.create(config);
+		}
+
+	}
+
+}
