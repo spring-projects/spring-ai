58642552b923b430601769468201dd187a757ad2
Soby Chacko <soby.chacko@broadcom.com>
2025-04-11 13:50:47 -0400

    Checkstyle fixes in spring-ai-commons
    Signed-off-by: Soby Chacko <soby.chacko@broadcom.com>

--- spring-ai-commons/src/test/java/org/springframework/ai/document/DocumentTests.java
+++ spring-ai-commons/src/test/java/org/springframework/ai/document/DocumentTests.java
@@ -23,8 +23,8 @@ import java.util.Map;
 
 import org.junit.jupiter.api.Test;
 
-import org.springframework.ai.document.id.IdGenerator;
 import org.springframework.ai.content.Media;
+import org.springframework.ai.document.id.IdGenerator;
 import org.springframework.util.MimeTypeUtils;
 
 import static org.assertj.core.api.Assertions.assertThat;
