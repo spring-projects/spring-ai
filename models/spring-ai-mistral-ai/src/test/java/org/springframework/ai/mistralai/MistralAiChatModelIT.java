b67fe95d03a21c59a3ee52aca35a65f8c51c7f53
Changho Kim <dukso4885@gmail.com>
2025-04-21 15:41:36 +0900

    fix: correct name typo from 'Evalutaor' to 'Evaluator'
    Signed-off-by: Changho Kim <dukso4885@gmail.com>

--- models/spring-ai-mistral-ai/src/test/java/org/springframework/ai/mistralai/MistralAiChatModelIT.java
+++ models/spring-ai-mistral-ai/src/test/java/org/springframework/ai/mistralai/MistralAiChatModelIT.java
@@ -84,7 +84,7 @@ class MistralAiChatModelIT {
 	protected Resource qaEvaluatorNotRelatedResource;
 
 	@Value("classpath:/prompts/eval/qa-evaluator-fact-based-answer.st")
-	protected Resource qaEvalutaorFactBasedAnswerResource;
+	protected Resource qaEvaluatorFactBasedAnswerResource;
 
 	@Value("classpath:/prompts/eval/user-evaluator-message.st")
 	protected Resource userEvaluatorResource;
