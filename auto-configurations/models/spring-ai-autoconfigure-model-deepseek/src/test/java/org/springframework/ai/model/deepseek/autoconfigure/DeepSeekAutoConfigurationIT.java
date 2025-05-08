5b8de38e7df8130f06812fb3051a015ddb532453
GR <gr@fastball.dev>
2025-05-08 10:07:24 +0800

    feat: add deepseek starter and docs
    Signed-off-by: GR <gr@fastball.dev>

---
+++ auto-configurations/models/spring-ai-autoconfigure-model-deepseek/src/test/java/org/springframework/ai/model/deepseek/autoconfigure/DeepSeekAutoConfigurationIT.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2023-2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.ai.model.deepseek.autoconfigure;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.deepseek.DeepSeekChatModel;
+import org.springframework.ai.retry.autoconfigure.SpringAiRetryAutoConfiguration;
+import org.springframework.boot.autoconfigure.AutoConfigurations;
+import org.springframework.boot.autoconfigure.web.client.RestClientAutoConfiguration;
+import org.springframework.boot.test.context.runner.ApplicationContextRunner;
+import reactor.core.publisher.Flux;
+
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author Geng Rong
+ */
+@EnabledIfEnvironmentVariable(named = "DEEPSEEK_API_KEY", matches = ".*")
+public class DeepSeekAutoConfigurationIT {
+
+	private static final Log logger = LogFactory.getLog(DeepSeekAutoConfigurationIT.class);
+
+	private final ApplicationContextRunner contextRunner = new ApplicationContextRunner()
+		.withPropertyValues("spring.ai.deepseek.apiKey=" + System.getenv("DEEPSEEK_API_KEY"))
+		.withConfiguration(AutoConfigurations.of(SpringAiRetryAutoConfiguration.class,
+				RestClientAutoConfiguration.class, DeepSeekChatAutoConfiguration.class));
+
+	@Test
+	void generate() {
+		this.contextRunner.run(context -> {
+			DeepSeekChatModel client = context.getBean(DeepSeekChatModel.class);
+			String response = client.call("Hello");
+			assertThat(response).isNotEmpty();
+			logger.info("Response: " + response);
+		});
+	}
+
+	@Test
+	void generateStreaming() {
+		this.contextRunner.run(context -> {
+			DeepSeekChatModel client = context.getBean(DeepSeekChatModel.class);
+			Flux<ChatResponse> responseFlux = client.stream(new Prompt(new UserMessage("Hello")));
+			String response = Objects.requireNonNull(responseFlux.collectList().block())
+				.stream()
+				.map(chatResponse -> chatResponse.getResults().get(0).getOutput().getText())
+				.collect(Collectors.joining());
+
+			assertThat(response).isNotEmpty();
+			logger.info("Response: " + response);
+		});
+	}
+
+}
---
+++ auto-configurations/models/spring-ai-autoconfigure-model-deepseek/src/test/java/org/springframework/ai/model/deepseek/autoconfigure/tool/DeepSeekFunctionCallbackIT.java
@@ -0,0 +1,126 @@
+/*
+ * Copyright 2023-2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.ai.model.deepseek.autoconfigure.tool;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.ai.chat.messages.AssistantMessage;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.model.Generation;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.deepseek.DeepSeekChatModel;
+import org.springframework.ai.deepseek.DeepSeekChatOptions;
+import org.springframework.ai.model.deepseek.autoconfigure.DeepSeekChatAutoConfiguration;
+import org.springframework.ai.retry.autoconfigure.SpringAiRetryAutoConfiguration;
+import org.springframework.ai.tool.ToolCallback;
+import org.springframework.ai.tool.function.FunctionToolCallback;
+import org.springframework.boot.autoconfigure.AutoConfigurations;
+import org.springframework.boot.autoconfigure.web.client.RestClientAutoConfiguration;
+import org.springframework.boot.test.context.runner.ApplicationContextRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Flux;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author Geng Rong
+ */
+// @Disabled("the deepseek-chat model's Function Calling capability is unstable see:
+// https://api-docs.deepseek.com/guides/function_calling")
+@EnabledIfEnvironmentVariable(named = "DEEPSEEK_API_KEY", matches = ".*")
+public class DeepSeekFunctionCallbackIT {
+
+	private final Logger logger = LoggerFactory.getLogger(DeepSeekFunctionCallbackIT.class);
+
+	private final ApplicationContextRunner contextRunner = new ApplicationContextRunner()
+		.withPropertyValues("spring.ai.deepseek.apiKey=" + System.getenv("DEEPSEEK_API_KEY"))
+		.withConfiguration(AutoConfigurations.of(SpringAiRetryAutoConfiguration.class,
+				RestClientAutoConfiguration.class, DeepSeekChatAutoConfiguration.class))
+		.withUserConfiguration(Config.class);
+
+	@Test
+	void functionCallTest() {
+		this.contextRunner.run(context -> {
+
+			DeepSeekChatModel chatModel = context.getBean(DeepSeekChatModel.class);
+
+			UserMessage userMessage = new UserMessage(
+					"What's the weather like in San Francisco, Tokyo, and Paris? Return the temperature in Celsius");
+
+			ChatResponse response = chatModel
+				.call(new Prompt(List.of(userMessage), DeepSeekChatOptions.builder().toolNames("WeatherInfo").build()));
+
+			logger.info("Response: {}", response);
+
+			assertThat(response.getResult().getOutput().getText()).contains("30", "10", "15");
+
+		});
+	}
+
+	@Test
+	void streamFunctionCallTest() {
+		this.contextRunner.run(context -> {
+
+			DeepSeekChatModel chatModel = context.getBean(DeepSeekChatModel.class);
+
+			UserMessage userMessage = new UserMessage(
+					"What's the weather like in San Francisco, Tokyo, and Paris? Return the temperature in Celsius");
+
+			Flux<ChatResponse> response = chatModel.stream(
+					new Prompt(List.of(userMessage), DeepSeekChatOptions.builder().toolNames("WeatherInfo").build()));
+
+			String content = response.collectList()
+				.block()
+				.stream()
+				.map(ChatResponse::getResults)
+				.flatMap(List::stream)
+				.map(Generation::getOutput)
+				.map(AssistantMessage::getText)
+				.filter(Objects::nonNull)
+				.collect(Collectors.joining());
+			logger.info("Response: {}", content);
+
+			assertThat(content).containsAnyOf("30.0", "30");
+			assertThat(content).containsAnyOf("10.0", "10");
+			assertThat(content).containsAnyOf("15.0", "15");
+
+		});
+	}
+
+	@Configuration
+	static class Config {
+
+		@Bean
+		public ToolCallback weatherFunctionInfo() {
+
+			return FunctionToolCallback.builder("WeatherInfo", new MockWeatherService())
+				.description("Get the weather in location")
+				.inputType(MockWeatherService.Request.class)
+				.build();
+		}
+
+	}
+
+}
