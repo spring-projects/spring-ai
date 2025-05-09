85dfc90014b4cc7f0c896f19aec8256c1d01f866
Temuu-jin <temucin.d.k@gmail.com>
2025-05-09 11:03:36 +0200

    fix indentation Signed-off-by: Temucin Damdinjamts-Kintaert <temucin.d.k@gmail.com>

--- auto-configurations/vector-stores/spring-ai-autoconfigure-vector-store-neo4j/src/main/java/org/springframework/ai/vectorstore/neo4j/autoconfigure/Neo4jVectorStoreAutoConfiguration.java
+++ auto-configurations/vector-stores/spring-ai-autoconfigure-vector-store-neo4j/src/main/java/org/springframework/ai/vectorstore/neo4j/autoconfigure/Neo4jVectorStoreAutoConfiguration.java
@@ -75,7 +75,7 @@ public class Neo4jVectorStoreAutoConfiguration {
 			.indexName(properties.getIndexName())
 			.idProperty(properties.getIdProperty())
 			.constraintName(properties.getConstraintName())
-				.textProperty(properties.getTextProperty())
+			.textProperty(properties.getTextProperty())
 			.build();
 	}
 
