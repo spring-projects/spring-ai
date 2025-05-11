67b48d009f333692bf68b5226c6a3d87c8fc9fe2
Tran Ngoc Nhan <ngocnhan.tran1996@gmail.com>
2025-05-11 21:55:54 +0700

    Fix typos
    Signed-off-by: Tran Ngoc Nhan <ngocnhan.tran1996@gmail.com>

--- auto-configurations/models/spring-ai-autoconfigure-model-vertex-ai/src/test/java/org/springframework/ai/model/vertexai/autoconfigure/gemini/tool/FunctionCallWithFunctionBeanIT.java
+++ auto-configurations/models/spring-ai-autoconfigure-model-vertex-ai/src/test/java/org/springframework/ai/model/vertexai/autoconfigure/gemini/tool/FunctionCallWithFunctionBeanIT.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023-2024 the original author or authors.
+ * Copyright 2023-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -65,7 +65,7 @@ class FunctionCallWithFunctionBeanIT {
 				var userMessage = new UserMessage("""
 						What's the weather like in San Francisco, Paris and in Tokyo?
 						Return the temperature in Celsius.
-						Perform multiple funciton execution if necessary.
+						Perform multiple function execution if necessary.
 						""");
 
 				ChatResponse response = chatModel.call(new Prompt(List.of(userMessage),
@@ -105,7 +105,7 @@ class FunctionCallWithFunctionBeanIT {
 				var userMessage = new UserMessage("""
 						What's the weather like in San Francisco, Paris and in Tokyo?
 						Return the temperature in Celsius.
-						Perform multiple funciton execution if necessary.
+						Perform multiple function execution if necessary.
 						""");
 
 				ChatResponse response = chatModel.call(new Prompt(List.of(userMessage),
