a5ecddbe1ac11121b7b414b707db7e56bddc6e5d
lambochen <lambochen@yeah.net>
2025-05-29 22:26:58 +0800

    fix: OpenAiChatOptions support transfer webSearchOptions
    Signed-off-by: lambochen <lambochen@yeah.net>

--- models/spring-ai-openai/src/main/java/org/springframework/ai/openai/OpenAiChatOptions.java
+++ models/spring-ai-openai/src/main/java/org/springframework/ai/openai/OpenAiChatOptions.java
@@ -266,6 +266,7 @@ public class OpenAiChatOptions implements ToolCallingChatOptions {
 			.store(fromOptions.getStore())
 			.metadata(fromOptions.getMetadata())
 			.reasoningEffort(fromOptions.getReasoningEffort())
+			.webSearchOptions(fromOptions.getWebSearchOptions())
 			.build();
 	}
 
