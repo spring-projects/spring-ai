7b4cc876736e8be79d306a536be3897664d68647
Yurii Kostyukov <kostyukov.yurii@gmail.com>
2025-06-06 12:17:57 +0300

    fix: DeepSeek

--- auto-configurations/models/spring-ai-autoconfigure-model-deepseek/src/main/java/org/springframework/ai/model/deepseek/autoconfigure/DeepSeekParentProperties.java
+++ auto-configurations/models/spring-ai-autoconfigure-model-deepseek/src/main/java/org/springframework/ai/model/deepseek/autoconfigure/DeepSeekParentProperties.java
@@ -43,8 +43,4 @@ public class DeepSeekParentProperties {
 		this.baseUrl = baseUrl;
 	}
 
-	public static Consumer<HttpHeaders> getJsonContentHeaders() {
-		return (headers) -> headers.setContentType(MediaType.APPLICATION_JSON);
-	}
-
 }
