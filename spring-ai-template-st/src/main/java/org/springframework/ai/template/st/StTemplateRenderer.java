669670d1d052c892266c939a6022daa2f82a9392
engineer <jiaxingengineer@gmail.com>
2025-07-16 21:03:25 +0800

    Refactor (template): Optimize template rendering and variable handling
    Signed-off-by: engineer <jiaxingengineer@gmail.com>

--- spring-ai-template-st/src/main/java/org/springframework/ai/template/st/StTemplateRenderer.java
+++ spring-ai-template-st/src/main/java/org/springframework/ai/template/st/StTemplateRenderer.java
@@ -1,19 +1,3 @@
-/**
- * Copyright 2023-2025 the original author or authors.
- * <p>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * <p>
- *      https://www.apache.org/licenses/LICENSE-2.0
- * <p>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package org.springframework.ai.template.st;
 
 import java.util.Collections;
@@ -169,7 +153,6 @@ public class StTemplateRenderer implements TemplateRenderer {
 		TokenStream tokens = st.impl.tokens;
 		boolean isInsideList = false;
 
-		// StringTemplate 控制结构关键字集合，校验时忽略它们
 		Set<String> stKeywords = Set.of("if", "elseif", "else", "endif", "for", "endfor", "while", "endwhile", "switch",
 				"endswitch", "case", "default");
 
@@ -210,6 +193,9 @@ public class StTemplateRenderer implements TemplateRenderer {
 				inputVariables.add(var);
 			}
 		}
+
+		Set<String> localVariables = Set.of("it", "item", "index", "key", "value");
+		inputVariables.removeAll(localVariables);
 
 		return inputVariables;
 	}
