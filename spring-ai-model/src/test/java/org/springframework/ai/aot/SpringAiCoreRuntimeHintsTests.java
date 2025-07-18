e2b28703db59833e27c5be2b3a547585fd09fa9c
academey <academey@gmail.com>
2025-07-18 11:27:29 +0900

    Update copyright year and author in SpringAiCoreRuntimeHintsTests
    - Update copyright year from 2023-2024 to 2023-2025
    - Remove @author Christian Tzolov as per contributor guidelines
    
    Signed-off-by: academey <academey@gmail.com>

--- spring-ai-model/src/test/java/org/springframework/ai/aot/SpringAiCoreRuntimeHintsTests.java
+++ spring-ai-model/src/test/java/org/springframework/ai/aot/SpringAiCoreRuntimeHintsTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023-2024 the original author or authors.
+ * Copyright 2023-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -37,7 +37,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 /**
  * Tests for {@link SpringAiCoreRuntimeHints}.
  *
- * @author Christian Tzolov
  * @author Hyunjoon Park
  */
 class SpringAiCoreRuntimeHintsTests {
