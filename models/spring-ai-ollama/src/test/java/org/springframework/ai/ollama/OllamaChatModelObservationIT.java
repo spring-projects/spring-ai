28e7f2d2b49bb96847311e291af7c27cfc58f769
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2025-08-01 17:55:40 +0200

    Update Ollama API to latest version
    * Add missing fields to Ollama API classes
    * Deprecate Ollama options that are no longer supported
    * Update integration tests using Ollama 0.10.1 (latest version) and more performant models.
    
    Fixes #3969
    
    Auto-cherry-pick to 1.0.x
    
    Signed-off-by: Thomas Vitale <ThomasVitale@users.noreply.github.com>
    Signed-off-by: 家娃 <guanjing.pangj@alibaba-inc.com>

--- models/spring-ai-ollama/src/test/java/org/springframework/ai/ollama/OllamaChatModelObservationIT.java
+++ models/spring-ai-ollama/src/test/java/org/springframework/ai/ollama/OllamaChatModelObservationIT.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023-2024 the original author or authors.
+ * Copyright 2023-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -53,7 +53,7 @@ import static org.springframework.ai.chat.observation.ChatModelObservationDocume
 @SpringBootTest(classes = OllamaChatModelObservationIT.Config.class)
 public class OllamaChatModelObservationIT extends BaseOllamaIT {
 
-	private static final String MODEL = OllamaModel.LLAMA3_2.getName();
+	private static final String MODEL = OllamaModel.QWEN_2_5_3B.getName();
 
 	@Autowired
 	TestObservationRegistry observationRegistry;
--- models/spring-ai-ollama/src/test/java/org/springframework/ai/ollama/api/OllamaApiIT.java
+++ models/spring-ai-ollama/src/test/java/org/springframework/ai/ollama/api/OllamaApiIT.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023-2024 the original author or authors.
+ * Copyright 2023-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,21 +16,16 @@
 
 package org.springframework.ai.ollama.api;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.ai.ollama.BaseOllamaIT;
+import org.springframework.ai.ollama.api.OllamaApi.*;
+import org.springframework.ai.ollama.api.OllamaApi.Message.Role;
+import reactor.core.publisher.Flux;
+
 import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
-
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import reactor.core.publisher.Flux;
-
-import org.springframework.ai.ollama.BaseOllamaIT;
-import org.springframework.ai.ollama.api.OllamaApi.ChatRequest;
-import org.springframework.ai.ollama.api.OllamaApi.ChatResponse;
-import org.springframework.ai.ollama.api.OllamaApi.EmbeddingsRequest;
-import org.springframework.ai.ollama.api.OllamaApi.EmbeddingsResponse;
-import org.springframework.ai.ollama.api.OllamaApi.Message;
-import org.springframework.ai.ollama.api.OllamaApi.Message.Role;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -40,16 +35,20 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 public class OllamaApiIT extends BaseOllamaIT {
 
-	private static final String MODEL = OllamaModel.LLAMA3_2.getName();
+	private static final String CHAT_MODEL = OllamaModel.QWEN_2_5_3B.getName();
+
+	private static final String EMBEDDING_MODEL = OllamaModel.NOMIC_EMBED_TEXT.getName();
+
+	private static final String THINKING_MODEL = OllamaModel.QWEN3_4B.getName();
 
 	@BeforeAll
 	public static void beforeAll() throws IOException, InterruptedException {
-		initializeOllama(MODEL);
+		initializeOllama(CHAT_MODEL, EMBEDDING_MODEL, THINKING_MODEL);
 	}
 
 	@Test
 	public void chat() {
-		var request = ChatRequest.builder(MODEL)
+		var request = ChatRequest.builder(CHAT_MODEL)
 			.stream(false)
 			.messages(List.of(
 					Message.builder(Role.SYSTEM)
@@ -67,7 +66,7 @@ public class OllamaApiIT extends BaseOllamaIT {
 		System.out.println(response);
 
 		assertThat(response).isNotNull();
-		assertThat(response.model()).contains(MODEL);
+		assertThat(response.model()).contains(CHAT_MODEL);
 		assertThat(response.done()).isTrue();
 		assertThat(response.message().role()).isEqualTo(Role.ASSISTANT);
 		assertThat(response.message().content()).contains("Sofia");
@@ -75,7 +74,7 @@ public class OllamaApiIT extends BaseOllamaIT {
 
 	@Test
 	public void streamingChat() {
-		var request = ChatRequest.builder(MODEL)
+		var request = ChatRequest.builder(CHAT_MODEL)
 			.stream(true)
 			.messages(List.of(Message.builder(Role.USER)
 				.content("What is the capital of Bulgaria and what is the size? " + "What it the national anthem?")
@@ -101,17 +100,45 @@ public class OllamaApiIT extends BaseOllamaIT {
 
 	@Test
 	public void embedText() {
-		EmbeddingsRequest request = new EmbeddingsRequest(MODEL, "I like to eat apples");
+		EmbeddingsRequest request = new EmbeddingsRequest(EMBEDDING_MODEL, "I like to eat apples");
 
 		EmbeddingsResponse response = getOllamaApi().embed(request);
 
 		assertThat(response).isNotNull();
 		assertThat(response.embeddings()).hasSize(1);
-		assertThat(response.embeddings().get(0)).hasSize(3072);
-		assertThat(response.model()).isEqualTo(MODEL);
+		assertThat(response.embeddings().get(0)).hasSize(768);
+		assertThat(response.model()).isEqualTo(EMBEDDING_MODEL);
 		assertThat(response.promptEvalCount()).isEqualTo(5);
 		assertThat(response.loadDuration()).isGreaterThan(1);
 		assertThat(response.totalDuration()).isGreaterThan(1);
+	}
+
+	@Test
+	public void think() {
+		var request = ChatRequest.builder(THINKING_MODEL)
+			.stream(false)
+			.messages(List.of(
+					Message.builder(Role.SYSTEM)
+						.content("You are geography teacher. You are talking to a student.")
+						.build(),
+					Message.builder(Role.USER)
+						.content("What is the capital of Bulgaria and what is the size? "
+								+ "What it the national anthem?")
+						.build()))
+			.options(OllamaOptions.builder().temperature(0.9).build())
+			.think(true)
+			.build();
+
+		ChatResponse response = getOllamaApi().chat(request);
+
+		System.out.println(response);
+
+		assertThat(response).isNotNull();
+		assertThat(response.model()).contains(THINKING_MODEL);
+		assertThat(response.done()).isTrue();
+		assertThat(response.message().role()).isEqualTo(Role.ASSISTANT);
+		assertThat(response.message().content()).contains("Sofia");
+		assertThat(response.message().thinking()).isNotEmpty();
 	}
 
 }
