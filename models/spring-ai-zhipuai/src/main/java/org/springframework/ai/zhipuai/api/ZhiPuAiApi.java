3e2ba848b52183db0e7c14ec4f894a384d3c4801
Tran Ngoc Nhan <ngocnhan.tran1996@gmail.com>
2025-08-06 08:20:35 +0700

    Remove unused import (#3921)
    Signed-off-by: Tran Ngoc Nhan <ngocnhan.tran1996@gmail.com>

--- models/spring-ai-zhipuai/src/main/java/org/springframework/ai/zhipuai/api/ZhiPuAiApi.java
+++ models/spring-ai-zhipuai/src/main/java/org/springframework/ai/zhipuai/api/ZhiPuAiApi.java
@@ -16,10 +16,8 @@
 
 package org.springframework.ai.zhipuai.api;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
