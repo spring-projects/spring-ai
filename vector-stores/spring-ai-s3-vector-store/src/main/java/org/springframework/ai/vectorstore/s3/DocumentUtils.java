a74d62e805161421e04af3b50a46be2ae9eb1464
Signed-off-by: matejnedic <matejnedic1@gmail.com>
2025-08-08 10:19:09 +0200

    Introduce docs and clean up
    Signed-off-by: matejnedic <matejnedic1@gmail.com>

--- vector-stores/spring-ai-s3-vector-store/src/main/java/org/springframework/ai/vectorstore/s3/DocumentUtils.java
+++ vector-stores/spring-ai-s3-vector-store/src/main/java/org/springframework/ai/vectorstore/s3/DocumentUtils.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.springframework.ai.vectorstore.s3;
 
 import software.amazon.awssdk.core.document.Document;
@@ -6,6 +21,12 @@ import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.util.*;
 
+
+/**
+ * Helper class to convert from AWS SDK Document to Object and vice versa.
+ *
+ * @author Matej Nedic
+ */
 public class DocumentUtils {
 
 	public static Document toDocument(Object obj) {
@@ -56,7 +77,7 @@ public class DocumentUtils {
 		Map<String, Document> mapDocs = document.asMap();
 		Map<String, Object> mapMetadata = new HashMap<>(mapDocs.size());
 		for (Map.Entry<String, Document> entry : mapDocs.entrySet()) {
-			mapMetadata.put(entry.getKey(), fromDocument(entry.getValue()));
+			mapMetadata.put(entry.getKey(), fromDocumentToObject(entry.getValue()));
 		}
 		return mapMetadata;
 	}
