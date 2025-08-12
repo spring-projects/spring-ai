f81a2f6897d718fc7951ef73bc0ae76fdebdebe1
Mark Pollack <mark.pollack@broadcom.com>
2025-08-11 22:53:27 -0400

    test: third validation of fast maintenance workflow
      Fixes #4098
    
    (cherry picked from commit 6b5b68cb05d982ff5f7d903fbfce1758c5ad6f57)

--- vector-stores/spring-ai-qdrant-store/src/test/java/org/springframework/ai/vectorstore/qdrant/QdrantObjectFactoryTests.java
+++ vector-stores/spring-ai-qdrant-store/src/test/java/org/springframework/ai/vectorstore/qdrant/QdrantObjectFactoryTests.java
@@ -27,7 +27,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 /**
  * Tests for {@link QdrantObjectFactory}.
  *
- * ignore: test 7 for github workflow trigger on commit.
+ * ignore: test 10 for github workflow trigger on commit.
  *
  * @author Heonwoo Kim
  */
