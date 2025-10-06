ffa6e1083043c90bf4a6063bd463441503c2e89e
Max Nigmatulin <40598909+emnigma@users.noreply.github.com>
2025-10-06 19:37:26 +0300

    fix: Extensive additional properties tests (#26)

--- models/spring-ai-openai/src/test/java/org/springframework/ai/openai/OpenAiChatModelCreateRequestTests.java
+++ models/spring-ai-openai/src/test/java/org/springframework/ai/openai/OpenAiChatModelCreateRequestTests.java
@@ -5,9 +5,18 @@
 package org.springframework.ai.openai;
 
 import java.util.List;
+import java.util.stream.Stream;
 
+import com.fasterxml.jackson.annotation.JsonClassDescription;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyDescription;
 import org.junit.jupiter.api.Test;
 
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.model.ModelOptionsUtils;
@@ -19,50 +28,159 @@ import org.springframework.ai.tool.definition.DefaultToolDefinition;
 import org.springframework.ai.model.tool.DefaultToolCallingManager;
 import org.springframework.ai.tool.definition.ToolDefinition;
 import org.springframework.ai.tool.function.FunctionToolCallback;
+import org.springframework.http.ResponseEntity;
 import org.springframework.retry.support.RetryTemplate;
 import io.micrometer.observation.ObservationRegistry;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonClassDescription("Weather API request")
+record MyMockWeatherServiceRequest(@JsonProperty(required = true,
+		value = "location") @JsonPropertyDescription("The city and state e.g. San Francisco, CA") String location,
+		@JsonProperty(required = true, value = "lat") @JsonPropertyDescription("The city latitude") double lat,
+		@JsonProperty(required = true, value = "lon") @JsonPropertyDescription("The city longitude") double lon,
+		@JsonProperty(value = "unit") @JsonPropertyDescription("Temperature unit") MockWeatherService.Unit unit) {
+
+}
+
+@EnabledIfEnvironmentVariable(named = "OPENAI_API_KEY", matches = ".+")
 public class OpenAiChatModelCreateRequestTests {
 
-	@Test
-	void createRequestWithStrictCompatiblePropertiesHasStrictTrue() {
-		var schema = """
-					{
-						"type": "object",
-						"properties": {
-							"target_file": {
-								"type": "string",
-								"description": "Path to the new file, relative to the project root directory."
-							},
-							"content": {
-								"type": "string",
-								"description": "The contents to write to the file. Can be an empty string to create an empty file."
-							}
-						},
-						"additionalProperties": false,
-						"required": ["target_file", "content"]
-					}
-				""";
+	private static Stream<Arguments> schemaProvider() {
+		return Stream.of(
+				// no 'additionalProperties', 'required' matches 'properties'
+				Arguments.of("""
+						{
+						  "type": "object",
+						  "properties": {
+						    "location": {
+						      "type": "string",
+						      "description": "The city and state e.g. San Francisco, CA"
+						    },
+						    "lat": {
+						      "type": "number",
+						      "description": "The city latitude"
+						    },
+						    "lon": {
+						      "type": "number",
+						      "description": "The city longitude"
+						    },
+						    "unit": {
+						      "type": "string",
+						      "enum": ["C", "F"]
+						    }
+						  },
+						  "required": ["location", "lat", "lon", "unit"]
+						}
+						""", null),
+				// 'additionalProperties' to 'false', 'required' matches 'properties'
+				Arguments.of("""
+						{
+						  "type": "object",
+						  "properties": {
+						    "location": {
+						      "type": "string",
+						      "description": "The city and state e.g. San Francisco, CA"
+						    },
+						    "lat": {
+						      "type": "number",
+						      "description": "The city latitude"
+						    },
+						    "lon": {
+						      "type": "number",
+						      "description": "The city longitude"
+						    },
+						    "unit": {
+						      "type": "string",
+						      "enum": ["C", "F"]
+						    }
+						  },
+						  "required": ["location", "lat", "lon", "unit"],
+						  "additionalProperties": false
+						}
+						""", true),
+				// no 'additionalProperties', 'required' do not match 'properties'
+				Arguments.of("""
+						{
+						  "type": "object",
+						  "properties": {
+						    "location": {
+						      "type": "string",
+						      "description": "The city and state e.g. San Francisco, CA"
+						    },
+						    "lat": {
+						      "type": "number",
+						      "description": "The city latitude"
+						    },
+						    "lon": {
+						      "type": "number",
+						      "description": "The city longitude"
+						    },
+						    "unit": {
+						      "type": "string",
+						      "enum": ["C", "F"]
+						    }
+						  },
+						  "required": ["location", "lat", "lon"]
+						}
+						""", null),
+				// 'additionalProperties' to true
+				Arguments.of("""
+						{
+						  "type": "object",
+						  "properties": {
+						    "location": {
+						      "type": "string",
+						      "description": "The city and state e.g. San Francisco, CA"
+						    },
+						    "lat": {
+						      "type": "number",
+						      "description": "The city latitude"
+						    },
+						    "lon": {
+						      "type": "number",
+						      "description": "The city longitude"
+						    },
+						    "unit": {
+						      "type": "string",
+						      "enum": ["C", "F"]
+						    }
+						  },
+						  "required": ["location", "lat", "lon", "unit"],
+						  "additionalProperties": true
+						}
+						""", null));
+	}
+
+	@ParameterizedTest
+	@MethodSource("schemaProvider")
+	void createRequestWithStrictCompatiblePropertiesHasStrictTrue(String schema, Boolean expectedStrictToBe) {
+
 		OpenAiChatOptions options = OpenAiChatOptions.builder()
 			.model(OpenAiApi.DEFAULT_CHAT_MODEL)
 			.toolCallbacks(List.of(FunctionToolCallback.builder("getCurrentWeather", new MockWeatherService())
 				.description("Get the weather in location")
 				.inputSchema(schema)
-				.inputType(MockWeatherService.Request.class)
+				.inputType(MyMockWeatherServiceRequest.class)
 				.build()))
 			.build();
 
-		Prompt prompt = new Prompt(List.of(new UserMessage("hello")), options);
+		Prompt prompt = new Prompt(List.of(new UserMessage("Get weather in SF, CA")), options);
 
 		OpenAiChatModel model = OpenAiChatModel.builder()
-			.openAiApi(OpenAiApi.builder().apiKey("sk-test").build())
+			.openAiApi(OpenAiApi.builder().apiKey(System.getenv("OPENAI_API_KEY")).build())
 			.build();
 
 		ChatCompletionRequest request = model.createRequest(prompt, false);
 
-		assertThat(request.tools().stream().allMatch(tool -> tool.getFunction().getStrict() == true)).isTrue();
+		assertThat(request.tools().stream().allMatch(tool -> tool.getFunction().getStrict() == expectedStrictToBe))
+			.isTrue();
+
+		assertThatCode(() -> {
+			var ignored = model.call(prompt);
+		}).doesNotThrowAnyException();
 	}
 
 }
