673f48366462515f37ea9645a2061f311e315950
guanxu <1510424541@qq.com>
2025-10-22 16:17:33 +0800

    Add Oracle and Sqlite support for JDBC chat memory
    - Add Oracle and Sqlite JdbcChatMemoryRepositoryDialect classes.
    - Modify the JdbcChatMemoryRepositoryDialect interface to support identifying the correct dialect for Oracle and SQLite by their database product names.
    - Add Oracle and Sqlite JdbcChatMemoryRepository integration test classes.
    
    Signed-off-by: guanxu <1510424541@qq.com>
    Signed-off-by: Eric Bottard <eric.bottard@broadcom.com>

--- memory/repository/spring-ai-model-chat-memory-repository-jdbc/src/main/java/org/springframework/ai/chat/memory/repository/jdbc/JdbcChatMemoryRepositoryDialect.java
+++ memory/repository/spring-ai-model-chat-memory-repository-jdbc/src/main/java/org/springframework/ai/chat/memory/repository/jdbc/JdbcChatMemoryRepositoryDialect.java
@@ -78,6 +78,7 @@ public interface JdbcChatMemoryRepositoryDialect {
 			case "MySQL", "MariaDB" -> new MysqlChatMemoryRepositoryDialect();
 			case "Microsoft SQL Server" -> new SqlServerChatMemoryRepositoryDialect();
 			case "HSQL Database Engine" -> new HsqldbChatMemoryRepositoryDialect();
+			case "SQLite" -> new SqliteChatMemoryRepositoryDialect();
 			default -> // Add more as needed
 				new PostgresChatMemoryRepositoryDialect();
 		};
