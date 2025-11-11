1cdfa35a9dc07dd3d2bb5343e74134588e1b1db7
Christian Tzolov <christian.tzolov@broadcom.com>
2025-11-11 18:52:37 +0100

    fix failing tests
    Signed-off-by: Christian Tzolov <christian.tzolov@broadcom.com>

--- models/spring-ai-openai/src/main/java/org/springframework/ai/openai/api/OpenAiApi.java
+++ models/spring-ai-openai/src/main/java/org/springframework/ai/openai/api/OpenAiApi.java
@@ -27,7 +27,6 @@ import java.util.stream.Collectors;
 
 import com.fasterxml.jackson.annotation.JsonAnyGetter;
 import com.fasterxml.jackson.annotation.JsonAnySetter;
-import com.fasterxml.jackson.annotation.JsonEnumDefaultValue;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
@@ -762,13 +761,11 @@ public class OpenAiApi {
 		/**
 		 * Handles, empty, NULL and unknown values
 		 */
-		@JsonEnumDefaultValue
 		@JsonProperty("")
 		UNKNOWN,
 		/**
 		 * The model hit a natural stop point or a provided stop sequence.
 		 */
-		@JsonEnumDefaultValue
 		@JsonProperty("stop")
 		STOP,
 		/**
