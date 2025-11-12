b35a332153ce2f5d34ff262766ec5b10050cb0ec
Yanming Zhou <zhouyanming@gmail.com>
2025-11-12 15:43:09 +0800

    Polish OpenAiChatAutoConfiguration and OpenAiEmbeddingAutoConfiguration
    1. Remove `embeddingsPath` setting from `OpenAiChatAutoConfiguration` since it is not used by chat model.
    2. Remove `completionsPath` setting from `OpenAiEmbeddingAutoConfiguration` since it is not used by embedding model.
    3. Reorder method parameters for consistency.
    
    See GH-4789
    
    Signed-off-by: Yanming Zhou <zhouyanming@gmail.com>

--- auto-configurations/models/spring-ai-autoconfigure-model-openai/src/main/java/org/springframework/ai/model/openai/autoconfigure/OpenAiEmbeddingAutoConfiguration.java
+++ auto-configurations/models/spring-ai-autoconfigure-model-openai/src/main/java/org/springframework/ai/model/openai/autoconfigure/OpenAiEmbeddingAutoConfiguration.java
@@ -69,7 +69,7 @@ public class OpenAiEmbeddingAutoConfiguration {
 			ObjectProvider<ObservationRegistry> observationRegistry,
 			ObjectProvider<EmbeddingModelObservationConvention> observationConvention) {
 
-		var openAiApi = openAiApi(embeddingProperties, commonProperties,
+		var openAiApi = openAiApi(commonProperties, embeddingProperties,
 				restClientBuilderProvider.getIfAvailable(RestClient::builder),
 				webClientBuilderProvider.getIfAvailable(WebClient::builder), responseErrorHandler, "embedding");
 
@@ -82,8 +82,8 @@ public class OpenAiEmbeddingAutoConfiguration {
 		return embeddingModel;
 	}
 
-	private OpenAiApi openAiApi(OpenAiEmbeddingProperties embeddingProperties,
-			OpenAiConnectionProperties commonProperties, RestClient.Builder restClientBuilder,
+	private OpenAiApi openAiApi(OpenAiConnectionProperties commonProperties,
+			OpenAiEmbeddingProperties embeddingProperties, RestClient.Builder restClientBuilder,
 			WebClient.Builder webClientBuilder, ObjectProvider<ResponseErrorHandler> responseErrorHandler,
 			String modelType) {
 
@@ -94,7 +94,6 @@ public class OpenAiEmbeddingAutoConfiguration {
 			.baseUrl(resolved.baseUrl())
 			.apiKey(new SimpleApiKey(resolved.apiKey()))
 			.headers(resolved.headers())
-			.completionsPath(OpenAiChatProperties.DEFAULT_COMPLETIONS_PATH)
 			.embeddingsPath(embeddingProperties.getEmbeddingsPath())
 			.restClientBuilder(restClientBuilder)
 			.webClientBuilder(webClientBuilder)
