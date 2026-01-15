72844216c5aa928206211f9bb42e25317c1f8e1e
Eric Bottard <eric.bottard@broadcom.com>
2026-01-15 17:06:54 +0100

    Add JSpecify annotations to MongoDB Atlas vector store module.
    Signed-off-by: Eric Bottard <eric.bottard@broadcom.com>

--- vector-stores/spring-ai-mongodb-atlas-store/src/main/java/org/springframework/ai/vectorstore/mongodb/atlas/VectorSearchAggregation.java
+++ vector-stores/spring-ai-mongodb-atlas-store/src/main/java/org/springframework/ai/vectorstore/mongodb/atlas/VectorSearchAggregation.java
@@ -22,14 +22,12 @@ import org.bson.Document;
 
 import org.springframework.data.mongodb.core.aggregation.AggregationOperation;
 import org.springframework.data.mongodb.core.aggregation.AggregationOperationContext;
-import org.springframework.lang.NonNull;
 
 record VectorSearchAggregation(List<Float> embeddings, String path, int numCandidates, String index, int count,
 		String filter) implements AggregationOperation {
 
-	@SuppressWarnings("null")
 	@Override
-	public org.bson.Document toDocument(@NonNull AggregationOperationContext context) {
+	public org.bson.Document toDocument(AggregationOperationContext context) {
 		var vectorSearch = new Document("queryVector", this.embeddings).append("path", this.path)
 			.append("numCandidates", this.numCandidates)
 			.append("index", this.index)
