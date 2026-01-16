a80c29de709d2fc96c0ce7d67d8f13ad43a6b7f3
Eric Bottard <eric.bottard@broadcom.com>
2026-01-16 11:12:12 +0100

    Add JSpecify annotations to Pinecone vector store module.
    Signed-off-by: Eric Bottard <eric.bottard@broadcom.com>

--- vector-stores/spring-ai-pinecone-store/src/main/java/org/springframework/ai/vectorstore/pinecone/PineconeVectorStoreHints.java
+++ vector-stores/spring-ai-pinecone-store/src/main/java/org/springframework/ai/vectorstore/pinecone/PineconeVectorStoreHints.java
@@ -18,6 +18,8 @@ package org.springframework.ai.vectorstore.pinecone;
 
 import java.util.Set;
 
+import org.jspecify.annotations.Nullable;
+
 import org.springframework.aot.hint.MemberCategory;
 import org.springframework.aot.hint.RuntimeHints;
 import org.springframework.aot.hint.RuntimeHintsRegistrar;
@@ -31,7 +33,7 @@ import org.springframework.aot.hint.RuntimeHintsRegistrar;
 class PineconeVectorStoreHints implements RuntimeHintsRegistrar {
 
 	@Override
-	public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
+	public void registerHints(RuntimeHints hints, @Nullable ClassLoader classLoader) {
 		for (var t : Set.of(com.google.protobuf.Value.class, com.google.protobuf.Value.Builder.class,
 				com.google.protobuf.Struct.class)) {
 			hints.reflection().registerType(t, MemberCategory.values());
