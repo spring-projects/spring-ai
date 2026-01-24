b652946730b2b868aa2ce745d4a4db684036877b
Khyojae <khjae201@gmail.com>
2026-01-25 00:24:41 +0900

    GH-5297: Fix Elasticsearch IN/NIN expression grouping syntax
    Signed-off-by: Khyojae <khjae201@gmail.com>

--- vector-stores/spring-ai-elasticsearch-store/src/main/java/org/springframework/ai/vectorstore/elasticsearch/ElasticsearchAiSearchFilterExpressionConverter.java
+++ vector-stores/spring-ai-elasticsearch-store/src/main/java/org/springframework/ai/vectorstore/elasticsearch/ElasticsearchAiSearchFilterExpressionConverter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023-2024 the original author or authors.
+ * Copyright 2023-2026 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -51,8 +51,8 @@ public class ElasticsearchAiSearchFilterExpressionConverter extends AbstractFilt
 	protected void doExpression(Expression expression, StringBuilder context) {
 		if (expression.type() == Filter.ExpressionType.IN || expression.type() == Filter.ExpressionType.NIN) {
 			context.append(getOperationSymbol(expression));
-			context.append("(");
 			this.convertOperand(expression.left(), context);
+			context.append("(");
 			this.convertOperand(expression.right(), context);
 			context.append(")");
 		}
@@ -116,7 +116,7 @@ public class ElasticsearchAiSearchFilterExpressionConverter extends AbstractFilt
 		if (filterValue.value() instanceof List list) {
 			int c = 0;
 			for (Object v : list) {
-				context.append(v);
+				this.doSingleValue(v, context);
 				if (c++ < list.size() - 1) {
 					this.doAddValueRangeSpitter(filterValue, context);
 				}
@@ -143,7 +143,7 @@ public class ElasticsearchAiSearchFilterExpressionConverter extends AbstractFilt
 				}
 			}
 			else {
-				context.append(text);
+				context.append("\"").append(text).append("\"");
 			}
 		}
 		else {
