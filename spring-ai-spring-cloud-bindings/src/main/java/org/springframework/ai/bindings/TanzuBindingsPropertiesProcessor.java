5dfee2317d0efe6730e8173a403433fb0e9d11fe
Sébastien Deleuze <sdeleuze@users.noreply.github.com>
2026-05-07 22:19:50 +0200

    Upgrade TanzuBindingsPropertiesProcessor for the new configuration properties
    Signed-off-by: Sébastien Deleuze <sdeleuze@users.noreply.github.com>
    Signed-off-by: R3gardless <pidaoh@g.skku.edu>

--- spring-ai-spring-cloud-bindings/src/main/java/org/springframework/ai/bindings/TanzuBindingsPropertiesProcessor.java
+++ spring-ai-spring-cloud-bindings/src/main/java/org/springframework/ai/bindings/TanzuBindingsPropertiesProcessor.java
@@ -49,12 +49,12 @@ public class TanzuBindingsPropertiesProcessor implements BindingsPropertiesProce
 				if (Arrays.stream(capabilities).anyMatch("chat"::equals)) {
 					properties.put("spring.ai.openai.chat.api-key", binding.getSecret().get("api-key"));
 					properties.put("spring.ai.openai.chat.base-url", binding.getSecret().get("uri"));
-					properties.put("spring.ai.openai.chat.options.model", binding.getSecret().get("model-name"));
+					properties.put("spring.ai.openai.chat.model", binding.getSecret().get("model-name"));
 				}
 				if (Arrays.stream(capabilities).anyMatch("embedding"::equals)) {
 					properties.put("spring.ai.openai.embedding.api-key", binding.getSecret().get("api-key"));
 					properties.put("spring.ai.openai.embedding.base-url", binding.getSecret().get("uri"));
-					properties.put("spring.ai.openai.embedding.options.model", binding.getSecret().get("model-name"));
+					properties.put("spring.ai.openai.embedding.model", binding.getSecret().get("model-name"));
 				}
 			}
 		});
