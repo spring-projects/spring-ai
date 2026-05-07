8307acd5e61f6041057b86dbd8343a1a3d428003
Ilayaperumal Gopinathan <ilayaperumal.gopinathan@broadcom.com>
2026-05-07 12:15:25 +0100

    Fix Qdrant image for the spring-boot-testcontainers
    Signed-off-by: Ilayaperumal Gopinathan <ilayaperumal.gopinathan@broadcom.com>
    Signed-off-by: R3gardless <pidaoh@g.skku.edu>

--- spring-ai-spring-boot-testcontainers/src/test/java/org/springframework/ai/testcontainers/service/connection/qdrant/QdrantImage.java
+++ spring-ai-spring-boot-testcontainers/src/test/java/org/springframework/ai/testcontainers/service/connection/qdrant/QdrantImage.java
@@ -23,7 +23,7 @@ import org.testcontainers.utility.DockerImageName;
  */
 public final class QdrantImage {
 
-	public static final DockerImageName DEFAULT_IMAGE = DockerImageName.parse("qdrant/qdrant:v1.9.7");
+	public static final DockerImageName DEFAULT_IMAGE = DockerImageName.parse("qdrant/qdrant:v1.13.0");
 
 	private QdrantImage() {
 
